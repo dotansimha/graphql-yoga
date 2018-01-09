d72c2d4612cac21108735892239e051c6ae0f3c1
James <metal.lunchbox@gmail.com>
2018-01-10 01:15:25 +0800

    add http to uri in console.log so that terminals know it's a link

--- examples/file-upload/index.ts
+++ examples/file-upload/index.ts
@@ -73,4 +73,4 @@ const resolvers = {
 
 const server = new GraphQLServer({ typeDefs, resolvers })
 
-server.start(() => console.log('Server is running on localhost:4000'))
+server.start(() => console.log('Server is running on http://localhost:4000'))
