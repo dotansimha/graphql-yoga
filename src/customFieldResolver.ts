883af9c514abd40c2449f6488cc188b40b8d1905
timsuchanek <tim.suchanek@gmail.com>
2018-04-06 17:37:50 +0200

    fix(format): customFieldResolver.ts

--- src/customFieldResolver.ts
+++ src/customFieldResolver.ts
@@ -1,18 +1,18 @@
 const customFieldResolver = (source, args, context, info) => {
   // ensure source is a value for which property access is acceptable.
-  if (typeof source === "object" || typeof source === "function") {
+  if (typeof source === 'object' || typeof source === 'function') {
     const property =
       source[
         info.fieldNodes[0].alias
           ? info.fieldNodes[0].alias.value
           : info.fieldName
-      ];
-    if (typeof property === "function") {
-      return source[info.fieldName](args, context, info);
+      ]
+    if (typeof property === 'function') {
+      return source[info.fieldName](args, context, info)
     }
-    return property;
+    return property
   }
-};
+}
 
-export default customFieldResolver;
-export { customFieldResolver };
+export default customFieldResolver
+export { customFieldResolver }
