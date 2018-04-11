ef1b1fd704199d00749243e225f8ea63f8507779
Daksh Miglani <hello@dak.sh>
2018-04-11 18:36:20 +0530

    cleanup main.js

--- examples/fullstack-vue/frontend/src/main.js
+++ examples/fullstack-vue/frontend/src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import VueApollo from 'vue-apollo';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
