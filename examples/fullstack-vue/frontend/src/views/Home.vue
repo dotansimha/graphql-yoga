46d932693207383409f03c9d8b5cf2f1a15e47e6
Daksh Miglani <hello@dak.sh>
2018-04-11 18:40:56 +0530

    update home.vue for items component

--- examples/fullstack-vue/frontend/src/views/Home.vue
+++ examples/fullstack-vue/frontend/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Items msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Items from '@/components/Items.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Items
   }
 }
 </script>
