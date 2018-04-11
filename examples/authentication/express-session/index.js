74bc79126758d31f65b050698e1a5835bbd2dd39
Daksh Miglani <hello@dak.sh>
2018-04-11 17:36:49 +0530

    resolvers

--- examples/authentication/express-session/index.js
+++ examples/authentication/express-session/index.js
@@ -9,7 +9,43 @@ const typeDefs = `
 
   type Mutation {
     login(username: String!, pwd: String!): Boolean!
+    signup(username: String!, pwd: String!): Boolean!
   }
 `
 
+const data = {};
+
+const resolvers = {
+  Query: {
+    isLogin: (parent, args, { req }) => typeof req.session.user !== 'undefined', 
+  },
+  Mutation: {
+    signup: async (parent, { username, pwd }, ctx) => {
+      if (data[username]) {
+        throw new Error('Another User with same username exists.');
+      }
+
+      data[username] = {
+        pwd: await bcrypt.hashSync(pwd, 10),
+      };
+      
+      return true;
+    },
+    login: async (parent, { username, pwd }, { req }) => {
+      const user = data[username];
+      if (user) {
+        if (await bcrypt.compareSync(pwd, user.pwd)) {
+          req.session.user = {
+            ...user
+          };
+          return true;
+        }
+
+        throw new Error('Incorrect password.');
+      }
+
+      throw new Error('No Such User exists.');
+    },
+  }
+}
 
