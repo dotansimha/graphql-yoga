119c95859b3ca41c246288c60e05b64d691077cb
Fabien BERNARD <fabien0102@hotmail.com>
2018-04-16 14:53:14 +0200

    Update apollo-engine example to deal with development env with engine.
    And format the example with prettier :100:

--- examples/apollo-engine/index.js
+++ examples/apollo-engine/index.js
@@ -1,5 +1,5 @@
-const { GraphQLServer, PubSub } = require("graphql-yoga");
-const { ApolloEngine } = require("apollo-engine");
+const { GraphQLServer, PubSub } = require('graphql-yoga')
+const { ApolloEngine } = require('apollo-engine')
 
 const typeDefs = `
   type Query {
@@ -14,54 +14,66 @@ const typeDefs = `
   type Subscription {
     counter: Counter!
   }
-`;
+`
 
 const resolvers = {
   Query: {
-    hello: (_, { name }) => `Hello ${name || "World"}`
+    hello: (_, { name }) => `Hello ${name || 'World'}`,
   },
   Counter: {
-    countStr: counter => `Current count: ${counter.count}`
+    countStr: counter => `Current count: ${counter.count}`,
   },
   Subscription: {
     counter: {
       subscribe: (parent, args, { pubsub }) => {
-        const channel = Math.random().toString(36).substring(2, 15); // random channel name
-        let count = 0;
+        const channel = Math.random()
+          .toString(36)
+          .substring(2, 15) // random channel name
+        let count = 0
         setInterval(
           () => pubsub.publish(channel, { counter: { count: count++ } }),
-          2000
-        );
-        return pubsub.asyncIterator(channel);
-      }
-    }
-  }
-};
+          2000,
+        )
+        return pubsub.asyncIterator(channel)
+      },
+    },
+  },
+}
 
-const pubsub = new PubSub();
-const port = parseInt(process.env.PORT, 10) || 4000;
+const pubsub = new PubSub()
+const port = parseInt(process.env.PORT, 10) || 4000
 const graphQLServer = new GraphQLServer({
   typeDefs,
   resolvers,
-  context: { pubsub }
-});
+  context: { pubsub },
+})
 
-const engine = new ApolloEngine({
-  apiKey: process.env.APOLLO_ENGINE_KEY
-});
+if (process.env.APOLLO_ENGINE_KEY) {
+  const engine = new ApolloEngine({
+    apiKey: process.env.APOLLO_ENGINE_KEY,
+  })
 
-const httpServer = graphQLServer.createHttpServer({
-  tracing: true,
-  cacheControl: true
-});
+  const httpServer = graphQLServer.createHttpServer({
+    tracing: true,
+    cacheControl: true,
+  })
 
-engine.listen(
-  {
-    port,
-    httpServer,
-    graphqlPaths: ["/"]
-  },
-  () =>
-    console.log(`Server is running on http://localhost:${port}`)
-);
-
+  engine.listen(
+    {
+      port,
+      httpServer,
+      graphqlPaths: ['/'],
+    },
+    () =>
+      console.log(
+        `Server with Apollo Engine is running on http://localhost:${port}`,
+      ),
+  )
+} else {
+  graphQLServer.start(
+    {
+      port,
+    },
+    () => console.log(`Server is running on http://localhost:${port}`),
+  )
+}
