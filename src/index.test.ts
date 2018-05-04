413b11803de857da898899f5a29a3e980003f8fb
Luis Alvarez <luisito453@gmail.com>
2018-05-04 13:38:36 -0500

    Added new test case for graphql-deduplicator

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,18 +1,47 @@
 import test from 'ava'
+import { inflate } from 'graphql-deduplicator'
 import { GraphQLServer } from './index'
 import { promisify } from 'util'
 import * as request from 'request-promise-native'
 
 test.beforeEach('start hello world', async t => {
+  const randomId = () => Math.random().toString(36).substr(2, 5)
+
   const typeDefs = `
+    type Author {
+      id: ID!
+      name: String!
+      lastName: String!
+    }
+
+    type Book {
+      id: ID!
+      name: String!
+      author: Author!
+    }
+
     type Query {
-        hello(name: String): String!
+      hello(name: String): String!
+      books: [Book!]!
     }
     `
 
+  const author = {
+    __typename: 'Author',
+    id: randomId(),
+    name: 'Jhon',
+    lastName: 'Doe'
+  }
+  const book = {
+    __typename: 'Book',
+    id: randomId(),
+    name: 'Awesome',
+    author
+  }
   const resolvers = {
     Query: {
       hello: (_, { name }) => `Hello ${name || 'World'}`,
+      books: () => Array(5).fill(book)
     },
   }
 
@@ -25,6 +54,7 @@ test.beforeEach('start hello world', async t => {
 
   t.context.http = http
   t.context.uri = uri
+  t.context.book = book
 })
 
 test.afterEach.always('stop hello world', async t => {
@@ -53,4 +83,61 @@ test('works with simple hello world server', async t => {
       hello: 'Hello Sarah',
     },
   })
+})
+
+test('Response data can be deduplicated with graphql-deduplicator', async t => {
+  const { uri, book } = t.context
+
+  const query = `
+    query {
+      books {
+        __typename
+        id
+        name
+        author {
+          __typename
+          id
+          name
+          lastName
+        }
+      }
+    }
+  `
+
+  const body = await request({
+    uri,
+    method: 'POST',
+    json: true,
+    body: { query },
+  }).promise()
+
+  const deduplicated = await request({
+    uri,
+    method: 'POST',
+    json: true,
+    body: { query },
+    headers: {
+      'X-GraphQL-Deduplicate': true
+    }
+  }).promise()
+
+  t.deepEqual(body, {
+    data: {
+      books: Array(5).fill(book)
+    }
+  })
+
+  t.deepEqual(deduplicated, {
+    data: {
+      books: [
+        book,
+        ...Array(4).fill({
+          __typename: book.__typename,
+          id: book.id
+        })
+      ]
+    }
+  })
+
+  t.deepEqual(body.data, inflate(deduplicated.data))
 })
