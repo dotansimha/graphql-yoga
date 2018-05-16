9d187827e0ad7a667a9e26f2598f40134b6a3ee6
Vasyl Boroviak <kore.sar@gmail.com>
2018-05-16 17:31:58 +1000

    types should allow array of IResolvers

--- src/types.ts
+++ src/types.ts
@@ -113,7 +113,7 @@ export interface Props {
     [name: string]: typeof SchemaDirectiveVisitor
   }
   typeDefs?: ITypeDefinitions
-  resolvers?: IResolvers
+  resolvers?: IResolvers | Array<IResolvers>
   resolverValidationOptions?: IResolverValidationOptions
   schema?: GraphQLSchema
   context?: Context | ContextCallback
@@ -127,7 +127,7 @@ export interface LambdaProps {
     [name: string]: typeof SchemaDirectiveVisitor
   }
   typeDefs?: string
-  resolvers?: IResolvers
+  resolvers?: IResolvers | Array<IResolvers>
   schema?: GraphQLSchema
   context?: Context | ContextCallback
   options?: LambdaOptions
