145d47523373c98e272dc76a37f3de762e1409cc
Daksh Miglani <hello@dak.sh>
2018-05-25 16:50:49 +0530

    resolvers done.

---
+++ examples/mongodb/src/graphql/resolvers.js
@@ -0,0 +1,34 @@
+export default {
+  Query: {
+    posts: async (parent, args, { models }) => {
+      const Posts = await models.Post.find({});
+      console.log(Posts);
+      return Posts;
+    },
+  },
+  Mutation: {
+    createPost: (parent, { title, desc, author }, { models }) => {
+      const Post = await models.Post.findOne({ title });
+
+      if (Post) {
+        throw new Error('Please provide a unique title.');        
+      }
+      
+      // create a new post
+      const newPost = new models.Post({
+        title,
+        desc,
+        author
+      });
+
+      // save the post
+      try {
+        await newPost.save();
+      } catch (e) {
+        throw new Error('Cannot Save Post!!!');
+      }
+
+      return true;
+    },
+  },
+};
