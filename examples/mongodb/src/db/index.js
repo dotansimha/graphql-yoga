702b94013232b77065e5c0e37f843cc355d9b8e3
Daksh Miglani <hello@dak.sh>
2018-05-25 16:27:34 +0530

    use es6, cuz i can't use commonjs

--- examples/mongodb/src/db/index.js
+++ examples/mongodb/src/db/index.js
@@ -1,13 +1,11 @@
-const mongoose = require('mongoose');
-const Post = require('./post.js');
+import mongoose from 'mongoose';
+import Post from './post.js';
 
 // SET UP Mongoose Promises.
 mongoose.Promise = global.Promise;
 
-module.exports = {
-  startDB: ({ user, pwd, url, db }) => 
-  mongoose.connect(`mongodb://${user}:${pwd}@${url}/${db}`),
-  models: {
-    Post
-  },
-};
+export const startDB = ({ user, pwd, url, db }) => mongoose.connect(`mongodb://${user}:${pwd}@${url}/${db}`);
+  
+export const models = {
+  Post,
+}
--- examples/mongodb/src/db/post.js
+++ examples/mongodb/src/db/post.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const PostSchema = new mongoose.Schema({
   title: {
@@ -20,4 +20,4 @@ const PostSchema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model('Post', PostSchema);
+export default mongoose.model('Post', PostSchema);
