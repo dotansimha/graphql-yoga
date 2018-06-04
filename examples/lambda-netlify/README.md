76128e83c6e6a08d1375ac2b1003fb5050e57338
Johannes Schickling <schickling.j@gmail.com>
2018-06-04 22:17:08 +0200

    added lambda-netlify example (readme missing)

---
+++ examples/lambda-netlify/README.md
@@ -0,0 +1,3 @@
+# lambda-netlify
+
+*Readme WIP*
---
+++ examples/lambda-netlify/server/index.js
@@ -0,0 +1,20 @@
+const { GraphQLServerLambda } = require('graphql-yoga')
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => `Hello ${name || 'world'}`,
+  },
+}
+
+const lambda = new GraphQLServerLambda({
+  typeDefs,
+  resolvers,
+})
+
+exports.handler = lambda.handler
