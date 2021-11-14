4fc7a70d8f4b3d89e4fe5b6c1fce65ab7c6fafeb
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-11-13 21:02:16 -0500

    build: setup jest

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+const { resolve } = require('path')
+const CI = !!process.env.CI
+
+const ROOT_DIR = __dirname
+const TSCONFIG = resolve(ROOT_DIR, 'tsconfig.json')
+const tsconfig = require(TSCONFIG)
+
+module.exports = {
+  testEnvironment: 'node',
+  rootDir: ROOT_DIR,
+  restoreMocks: true,
+  reporters: ['default'],
+  // TODO: get tests for `examples`
+  modulePathIgnorePatterns: ['dist', 'examples'],
+  moduleNameMapper: {
+    '@graphql-yoga/core': '<rootDir>/packages/core/src/index.ts',
+    '@graphql-yoga/handler': '<rootDir>/packages/handler/src/index.ts',
+  },
+  collectCoverage: true,
+  cacheDirectory: resolve(ROOT_DIR, `${CI ? '' : 'node_modules/'}.cache/jest`),
+}
