834c9e1ebee30f91e6ea08d5e2245c71aa9de45f
Laurin Quast <laurinquast@googlemail.com>
2021-12-17 18:28:46 +0100

    defer/stream example (#744)

---
+++ examples/defer-stream/README.md
@@ -0,0 +1,25 @@
+# defer and stream
+
+This directory contains a simple GraphQL defer/stream example based on `graphql-yoga`.
+
+## Get started
+
+**Clone the repository:**
+
+```sh
+git clone https://github.com/graphcool/graphql-yoga.git
+cd graphql-yoga/examples/defer-stream
+```
+
+**Install dependencies and run the app:**
+
+```sh
+yarn install # or npm install
+yarn start   # or npm start
+```
+
+## Testing
+
+### Counter
+
+Visit [http://localhost:4000](http://localhost:4000) i your browser and play around with the operations.
---
+++ examples/defer-stream/src/index.ts
@@ -0,0 +1,129 @@
+import { createGraphQLServer } from "graphql-yoga";
+
+const wait = (time: number) => new Promise((resolve) => setTimeout(resolve, time))
+
+const typeDefs = /* GraphQL */ `
+  type Query {
+    """
+    Resolves the alphabet slowly. 1 character per second
+    Maybe you want to @stream this field ;)
+    """
+    alphabet: [String]
+
+    """
+    A field that resolves fast.
+    """
+    fastField: String!
+    
+
+    """
+    A field that resolves a bit slow.
+    Maybe you want to @defer this field ;)
+    """
+    mediumSlowField: String!
+
+    """
+    A field that resolves slowly.
+    Maybe you want to @defer this field ;)
+    """
+    slowField: String!
+    
+  }
+`;
+
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+]
+
+const resolvers = {
+  Query: {
+    alphabet: async function* () {
+      for (const character of alphabet) {
+        yield character
+        await wait(1000)
+      }
+    },
+    fastField: async () => {
+      await wait(100)
+      return "I am speed"
+    },
+    mediumSlowField: async () => {
+      await wait(2000)
+      return "Just a bit late, right?"
+    },
+    slowField: async() => {
+      await wait(5000)
+      return "I am slow"
+    },
+  },
+};
+
+const server = createGraphQLServer({
+  typeDefs,
+  resolvers,
+  graphiql: {
+    defaultQuery: /* GraphQL */ `
+      # Slow alphabet
+      query Alphabet {
+        alphabet
+      }
+
+      # Stream Alphabet
+      query StreamAlphabet {
+        alphabet @stream
+      }
+
+      # Quick field
+      query QuickFieldOnly {
+        fastField
+      }
+
+      # Slow field
+      query SlowFieldOnly {
+        slowField
+      }
+
+      # Slow and Fast field
+      query SlowAndFastField {
+        slowField
+        fastField
+      }
+
+      # Slow and Fast using defer
+      query SlowAndFastFieldWithDefer {
+        ... on Query @defer {
+          slowField
+        }
+        fastField
+      }
+
+    `.split("\n").map(line => line.replace("      ", "")).join("\n")
+  } 
+});
+
+server.start();
