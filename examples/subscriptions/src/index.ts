d5b2315102f8a031632834bce54462d5f5225044
Laurin Quast <laurinquast@googlemail.com>
2021-12-17 13:33:19 +0100

    cross platform events (#736)
    * use EventTarget and Event for the PubSub implementation
    
    * user shall polyfill
    
    * fix typo
    
    * remove polyfill type def
    
    * move pubSub creation out of context
    
    * use push-pull-async-iterable-iterator for cleanup handling and improve the example
    
    * docs: recommend using Node.js 16

--- examples/subscriptions/src/index.ts
+++ examples/subscriptions/src/index.ts
@@ -1,39 +1,78 @@
 import { GraphQLServer, useExtendContext, createChannelPubSub } from "graphql-yoga";
 
-const typeDefs = `
+const wait = (time: number) => new Promise((resolve) => setTimeout(resolve, time))
+
+const typeDefs = /* GraphQL */ `
   type Query {
+    """
+    Simple field that return a "Hello world!" string.
+    """
     hello: String!
   }
 
   type Subscription {
+    """
+    Count up from 0 to Infinity.
+    """
     counter: Int!
+    """
+    Subscribe to the global counter that can be incremented with the 'incrementGlobalCounter' mutation.
+    """
+    globalCounter: Int!
+  }
+
+  type Mutation {
+    """
+    Increment the global counter by one. Returns the current global counter value after incrementing.
+    """
+    incrementGlobalCounter: Int!
   }
 `;
 
+let globalCounter = 0
+
 const resolvers = {
   Query: {
-    hello: () => `Hello`,
+    hello: () => `Hello world!`,
   },
   Subscription: {
     counter: {
-      subscribe: (_, __, { pubSub, logger }) => {
-        // Create a random channel so each request gets its own subscription.
-        // In real life, this can be dependent on the user's session.
-        const channelName = `counter:${(Math.random() * 2).toString()}`;
-        let count = 0;
-        logger.info(`Subscribing to ${channelName}`);
-        setInterval(() => pubSub.publish(channelName, count++), 1000);
-        return pubSub.subscribe(channelName);
+      subscribe: async function* () {
+        let counter = 0;
+
+        // count up until the subscription is terminated
+        while (true) {
+          yield counter++
+          await wait(1000)
+        }
+      },
+      resolve: (payload) => payload,
+    },
+    globalCounter: {
+      subscribe: async function * (_source, _args, context) {
+        // as soon as a client subscribes to this field we want to send him the latest value.
+        yield globalCounter
+        // after the initial value has been sent, we send new values to the client as the counter has been incremented.
+        yield* context.pubSub.subscribe("global:counter")
       },
       resolve: (payload) => payload,
     },
   },
+  Mutation: {
+    incrementGlobalCounter: (_source, _args, context) => {
+      globalCounter++
+      context.pubSub.publish("global:counter", globalCounter)
+      return globalCounter
+    }
+  },
 };
+
+const pubSub = createChannelPubSub()
 
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
-  plugins: [useExtendContext(() => ({ pubSub: createChannelPubSub() }))],
+  plugins: [useExtendContext(() => ({ pubSub }))],
 });
 
 server.start();
