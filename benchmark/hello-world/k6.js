1171280bf7ba17ce7d5f3e2b047e9fa28446ffad
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-12-20 16:27:41 +0300

    Go

--- benchmark/hello-world/k6.js
+++ benchmark/hello-world/k6.js
@@ -1,5 +1,7 @@
 import { check } from 'k6'
 import http from 'k6/http'
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js'
+import { githubComment } from 'https://raw.githubusercontent.com/dotansimha/k6-github-pr-comment/master/lib.js'
 
 export const options = {
   vus: 1,
