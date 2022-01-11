680c35ebf5a096d4b23e45164f556194d5c3bf72
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-01-11 13:40:13 +0300

    fix(fastify-example): add missing node typings and workaround for Readable.from

--- examples/fastify/src/app.ts
+++ examples/fastify/src/app.ts
@@ -43,7 +43,7 @@ export function buildApp() {
             });
 
             reply.status(response.status);
-            const nodeStream = Readable.from(response.body);
+            const nodeStream = Readable.from(response.body as any);
             reply.send(nodeStream);
         }
     })
