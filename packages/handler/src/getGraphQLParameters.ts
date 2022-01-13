389c4cbceda26500e04110851d9780e262b9d6bb
Laurin Quast <laurinquast@googlemail.com>
2022-01-13 09:59:40 +0100

    refactor: modularize getGraphQLParameters

--- packages/handler/src/getGraphQLParameters.ts
+++ packages/handler/src/getGraphQLParameters.ts
@@ -1,52 +1,93 @@
-import { GraphQLParams } from './types'
 import { dset } from 'dset'
 
-export async function getGraphQLParameters(
-  request: Request,
-): Promise<GraphQLParams> {
-  let operationName: string | undefined
-  let query: string | undefined
-  let variables: any | undefined
+type GraphQLRequestPayload = {
+  operationName?: string
+  query?: string
+  variables?: Record<string, unknown>
+  extensions?: Record<string, unknown>
+}
 
-  switch (request.method) {
-    case 'GET': {
-      const url = new URL(request.url)
-      operationName = url.searchParams.get('operationName') || undefined
-      query = url.searchParams.get('query') || undefined
-      variables = url.searchParams.get('variables') || undefined
-      break
+type RequestParser = {
+  is: (request: Request) => boolean
+  parse: (request: Request) => Promise<GraphQLRequestPayload>
+}
+
+export const GETRequestParser: RequestParser = {
+  is: (request) => request.method === 'GET',
+  parse: async (request) => {
+    const url = new URL(request.url)
+    const operationName = url.searchParams.get('operationName') || undefined
+    const query = url.searchParams.get('query') || undefined
+    const variables = url.searchParams.get('variables') || undefined
+    const extensions = url.searchParams.get('extensions') || undefined
+    return {
+      operationName,
+      query,
+      variables: variables ? JSON.parse(variables) : undefined,
+      extensions: extensions ? JSON.parse(extensions) : undefined,
     }
-    case 'POST': {
-      const contentType =
-        request.headers.get('content-type') || 'application/json'
-      if (contentType.startsWith('multipart/form-data')) {
-        const requestBody = await request.formData()
-        const operationsStr = requestBody.get('operations')?.toString() || '{}'
-        const operations = JSON.parse(operationsStr)
+  },
+}
 
-        const mapStr = requestBody.get('map')?.toString() || '{}'
-        const map = JSON.parse(mapStr)
-        for (const fileIndex in map) {
-          const file = requestBody.get(fileIndex)
-          const [path] = map[fileIndex]
-          dset(operations, path, file)
-        }
-        operationName = operations.operationName
-        query = operations.query
-        variables = operations.variables
-      } else {
-        const requestBody = await request.json()
-        operationName = requestBody?.operationName
-        query = requestBody?.query
-        variables = requestBody?.variables
+export const POSTRequestParser: RequestParser = {
+  is: (request) => request.method === 'POST',
+  parse: async (request) => {
+    const requestBody = await request.json()
+    return {
+      operationName: requestBody.operationName,
+      query: requestBody.query,
+      variables: requestBody.variables,
+      extensions: requestBody.extensions,
+    }
+  },
+}
+
+export const POSTMultipartFormDataRequestParser: RequestParser = {
+  is: (request) =>
+    request.method === 'POST' &&
+    !!request.headers.get('content-type')?.startsWith('multipart/form-data'),
+  parse: async (request) => {
+    const requestBody = await request.formData()
+    const operationsStr = requestBody.get('operations')?.toString() || '{}'
+    const operations = JSON.parse(operationsStr)
+
+    const mapStr = requestBody.get('map')?.toString() || '{}'
+    const map = JSON.parse(mapStr)
+    for (const fileIndex in map) {
+      const file = requestBody.get(fileIndex)
+      const [path] = map[fileIndex]
+      dset(operations, path, file)
+    }
+
+    return {
+      operationName: operations.operationName,
+      query: operations.query,
+      variables: operations.variables,
+      extensions: operations.extensions,
+    }
+  },
+}
+
+export function buildGetGraphQLParameters(parsers: Array<RequestParser>) {
+  return async function getGraphQLParameters(
+    request: Request,
+  ): Promise<GraphQLRequestPayload> {
+    for (const parser of parsers) {
+      if (parser.is(request)) {
+        return parser.parse(request)
       }
-      break
     }
-  }
-
-  return {
-    operationName,
-    query,
-    variables,
+    return {
+      operationName: undefined,
+      query: undefined,
+      variables: undefined,
+      extensions: undefined,
+    }
   }
 }
+
+export const getGraphQLParameters = buildGetGraphQLParameters([
+  GETRequestParser,
+  POSTMultipartFormDataRequestParser,
+  POSTRequestParser,
+])
