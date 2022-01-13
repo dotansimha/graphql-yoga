933c8804a052db876d63fcc85a70b456cbc89dc1
Laurin Quast <laurinquast@googlemail.com>
2022-01-13 18:23:54 +0100

    add giscus

--- website/src/pages/docs/[[...slug]].tsx
+++ website/src/pages/docs/[[...slug]].tsx
@@ -1,11 +1,14 @@
 import Head from 'next/head'
+import * as React from 'react'
 
 import { DocsContent, DocsTOC, MDXPage } from '@guild-docs/client'
 import { MDXPaths, MDXProps } from '@guild-docs/server'
+import Script from 'next/script'
 
 import { getRoutes } from '../../../routes'
 
 import type { GetStaticPaths, GetStaticProps } from 'next'
+import { useRouter } from 'next/router'
 
 export default MDXPage(function PostPage({
   content,
@@ -16,7 +19,11 @@ export default MDXPage(function PostPage({
   return (
     <>
       <Head>{MetaHead}</Head>
-      <DocsContent>{content}</DocsContent>
+      <DocsContent>
+        {content}
+        <div style={{ height: 10 }} />
+        <Giscus />
+      </DocsContent>
       <DocsTOC>
         <TOC />
         <BottomNavigation />
@@ -39,4 +46,37 @@ export const getStaticProps: GetStaticProps = (ctx) => {
 
 export const getStaticPaths: GetStaticPaths = (ctx) => {
   return MDXPaths('docs', { ctx })
+}
+
+function Giscus() {
+  const router = useRouter()
+  React.useEffect(() => {
+    const iframe = document.querySelector<HTMLIFrameElement>(
+      'iframe.giscus-frame',
+    )
+    iframe?.contentWindow?.postMessage(
+      { giscus: { setConfig: { term: router.asPath.split('?')[0] } } },
+      'https://giscus.app',
+    )
+  }, [router.asPath])
+
+  return (
+    <>
+      <Script
+        src="https://giscus.app/client.js"
+        data-repo="dotansimha/graphql-yoga"
+        data-repo-id="MDEwOlJlcG9zaXRvcnkxMTA4MTk5Mzk="
+        data-category="Docs Discussion"
+        data-category-id="DIC_kwDOBpr6Y84CAquY"
+        data-mapping="pathname"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-theme="preferred_color_scheme"
+        data-lang="en"
+        crossOrigin="anonymous"
+        async
+      />
+      <div className="giscus" />
+    </>
+  )
 }
