79f16dccb094b2ce0247cd486512072b158c0e74
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-01-14 13:33:40 +0300

    Apollo Federation Example

---
+++ examples/apollo-federation/service/index.js
@@ -0,0 +1,35 @@
+const { parse } = require('graphql')
+const { buildSubgraphSchema } = require('@apollo/subgraph')
+const { createServer } = require('graphql-yoga')
+
+const typeDefs = parse(/* GraphQL */ `
+  type Query {
+    me: User
+  }
+
+  type User @key(fields: "id") {
+    id: ID!
+    username: String
+  }
+`)
+
+const resolvers = {
+  Query: {
+    me() {
+      return { id: '1', username: '@ava' }
+    },
+  },
+  User: {
+    __resolveReference(user, { fetchUserById }) {
+      return fetchUserById(user.id)
+    },
+  },
+}
+const server = createServer({
+  schema: buildSubgraphSchema([{ typeDefs, resolvers }]),
+  port: 4001,
+})
+
+server.start().then(() => {
+  console.log(`🚀 Server ready at http://localhost:4001`)
+})
