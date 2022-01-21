492ecc1d380e43b132c3043ad6ea35e989394a37
jycouet <jycouet@gmail.com>
2022-01-21 16:49:26 +0100

    :sparkles: UPDATE: sveltekit next.234 & Yoga alpha6 (example and doc)

--- examples/sveltekit/src/routes/api/graphql.ts
+++ examples/sveltekit/src/routes/api/graphql.ts
@@ -1,6 +1,6 @@
+import { useGraphQlJit } from '@envelop/graphql-jit';
 import { createServer } from '@graphql-yoga/common';
-import type { ResponseHeaders } from '@sveltejs/kit/types/helper';
-import type { ServerRequest } from '@sveltejs/kit/types/hooks';
+import type { RequestEvent } from '@sveltejs/kit/types/hooks';
 
 export async function get() {
 	return {
@@ -9,38 +9,32 @@ export async function get() {
 	};
 }
 
-const graphQLServer = createServer({
-	typeDefs: `
-		type Query {
-			hello: String
+const yogaApp = createServer({
+	logging: false,
+	maskedErrors: process.env.NODE_ENV === 'development',
+	schema: {
+		typeDefs: `
+			type Query {
+				hello: String
+			}
+		`,
+		resolvers: {
+			Query: {
+				hello: () => 'SvelteKit - GraphQL Yoga'
+			}
 		}
-	`,
-	resolvers: {
-		Query: {
-			hello: () => 'GraphQL Yoga - SvelteKit'
-		}
-	}
+	},
+	plugins: [
+		useGraphQlJit()
+		// other plugins: https://www.envelop.dev/plugins
+	]
 });
 
-export async function post(req: ServerRequest) {
-	const request = new Request(req.url.toString(), {
-		method: req.method,
-		headers: req.headers,
-		body: req.rawBody
-	});
-	const response: Response = await graphQLServer.handleRequest(request);
-
-	const responseHeaders: ResponseHeaders = {};
-
-	response.headers.forEach((value, key) => {
-		responseHeaders[key] = value;
-	});
-
-	const responseBodyText = await response.text();
-
+export async function post(event: RequestEvent) {
+	const response: Response = await yogaApp.handleRequest(event.request);
 	return {
 		status: response.status,
-		headers: responseHeaders,
-		body: responseBodyText
+		headers: new Headers(response.headers),
+		body: await response.json()
 	};
 }
--- examples/sveltekit/src/routes/index.svelte
+++ examples/sveltekit/src/routes/index.svelte
@@ -1,3 +1,27 @@
-<h1>Welcome to GraphQL Yoga - SvelteKit</h1>
+<h1>
+	Welcome to <span class="svelte">SvelteKit</span> - <span class="graphql-yoga">GraphQL Yoga</span>
+</h1>
 
-<a rel="external" href="/api/graphiql">GraphiQL</a>
+<span>
+	Endpoint GraphQL (post)
+	<pre>/api/graphql</pre>
+</span>
+
+<span>
+	Endpoint Graph<i>i</i>QL (get)
+	<pre><a rel="external" href="/api/graphiql">/api/graphiql</a></pre>
+</span>
+
+<style>
+	pre {
+		margin-left: 2rem;
+	}
+	.svelte {
+		color: #ff3e00;
+	}
+	.graphql-yoga {
+		background: linear-gradient(to right top, #1cc8ee, #7433ff, #ffa3fd, #e10098);
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+	}
+</style>
