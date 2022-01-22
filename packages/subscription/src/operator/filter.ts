2b913f17ae2afc48618dc37727ac779375ecfc99
Laurin Quast <laurinquast@googlemail.com>
2022-01-22 13:53:28 +0100

    feat: allow any AsyncIterable source for map and fiter operators

--- packages/subscription/src/operator/filter.ts
+++ packages/subscription/src/operator/filter.ts
@@ -5,19 +5,22 @@ import { Repeater } from '@repeaterjs/repeater'
  */
 export function filter<T, U extends T>(
   filter: (input: T) => input is U,
-): (source: Repeater<T>) => Repeater<U, void, unknown>
+): (source: AsyncIterable<T>) => Repeater<U, void, unknown>
 export function filter<T>(
   filter: (input: T) => boolean,
-): (source: Repeater<T>) => Repeater<T, void, unknown>
+): (source: AsyncIterable<T>) => Repeater<T, void, unknown>
 export function filter(filter: (value: unknown) => boolean) {
-  return (source: Repeater<unknown>) =>
+  return (source: AsyncIterable<unknown>) =>
     new Repeater(async (push, stop) => {
+      const iterable = source[Symbol.asyncIterator]()
       stop.then(() => {
-        source.return()
+        iterable.return?.()
       })
-      for await (const value of source) {
-        if (filter(value)) {
-          push(value)
+
+      let latest: IteratorResult<unknown>
+      while ((latest = await iterable.next()).done === false) {
+        if (filter(latest.value)) {
+          await push(latest.value)
         }
       }
     })
