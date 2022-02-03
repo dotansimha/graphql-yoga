c09b15f8ef588470368948d84f93478e4e0a4358
Laurin Quast <laurinquast@googlemail.com>
2022-02-03 14:01:40 +0100

    use yarn1 thx

--- scripts/fix-bin.js
+++ scripts/fix-bin.js
@@ -7,14 +7,19 @@ const {
 } = require('fs-extra')
 const { resolve, join } = require('path')
 
-const absoluteExamplesDirPath = resolve(__dirname, '../examples')
+const absoluteExamplesDirPath = resolve(__dirname, '..', 'examples')
 const absoluteGraphqlYogaBinPath = resolve(
   __dirname,
-  '../packages/cli/dist/bin.js',
+  '..',
+  'packages',
+  'cli',
+  'dist',
+  'bin.js',
 )
 const dir = readdirSync(absoluteExamplesDirPath)
 for (const path of dir) {
   const absolutePath = join(absoluteExamplesDirPath, path)
+  console.log(absolutePath)
   if (lstatSync(absolutePath).isDirectory()) {
     const execNames = ['yoga', 'gql-yoga', 'graphql-yoga']
     for (const execName of execNames) {
