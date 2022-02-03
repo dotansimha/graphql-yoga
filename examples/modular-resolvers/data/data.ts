cb212958ed494eeda95a0abcfeafd92f356ce03f
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2022-02-03 14:40:33 -0500

    remove modular resolvers example

--- examples/modular-resolvers/data/data.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  users: [
-    { id: 1, userName: 'john', firstName: 'John', lastName: 'Smith' },
-    { id: 2, userName: 'kim', firstName: 'Kimberly', lastName: 'Jones' },
-  ],
-}
