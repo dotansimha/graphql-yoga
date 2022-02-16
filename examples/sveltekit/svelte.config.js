b4d451a82dc0335a2d2e3359f46aeda43e732b41
JYC <jycouet@gmail.com>
2022-02-16 16:49:20 +0100

    sveltekit 270 & One liner fix (#890)
    * :bug: FIX: sveltekit config breaking change
    
    * :bug: FIX: sveltekit one liner fix

--- examples/sveltekit/svelte.config.js
+++ examples/sveltekit/svelte.config.js
@@ -3,15 +3,10 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		adapter: adapter()
 	}
 };
 
