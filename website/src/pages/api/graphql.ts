b319d1c8212bc1a4881efef6735fc55630ee2371
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2022-02-22 01:34:59 -0500

    setup api example for docs (#888)

---
+++ website/src/pages/api/graphql.ts
@@ -0,0 +1,16 @@
+import { createServer } from '@graphql-yoga/node'
+
+const server = createServer({
+  cors: false,
+  endpoint: '/api/graphql',
+  logging: false,
+})
+
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
+}
+
+export default server.requestListener
