6e1f8506a7fedcff898973caf579f25e2c5912b6
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-02-25 01:38:25 +0300

    Koa example

---
+++ examples/koa/src/app.ts
@@ -0,0 +1,59 @@
+import { createServer } from '@graphql-yoga/node'
+import Koa from 'koa'
+import { Readable } from 'stream'
+
+export function buildApp() {
+  const app = new Koa()
+
+  const graphQLServer = createServer({
+    schema: {
+      typeDefs: /* GraphQL */ `
+        type Query {
+          hello: String
+        }
+        type Subscription {
+          countdown(from: Int!): Int!
+        }
+      `,
+      resolvers: {
+        Query: {
+          hello: () => 'world',
+        },
+        Subscription: {
+          countdown: {
+            subscribe: async function* (_, { from }) {
+              for (let i = from; i >= 0; i--) {
+                await new Promise((resolve) => setTimeout(resolve, 1000))
+                yield { countdown: i }
+              }
+            },
+          },
+        },
+      },
+    },
+    logging: false,
+  })
+
+  app.use(async (ctx) => {
+    // Keep the socket open
+    ctx.req.socket.setTimeout(0)
+    ctx.req.socket.setNoDelay(true)
+    ctx.req.socket.setKeepAlive(true)
+
+    const response = await graphQLServer.handleIncomingMessage(ctx.req)
+
+    // Set status code
+    ctx.status = response.status
+
+    // Set headers
+    response.headers.forEach((value, key) => {
+      ctx.append(key, value)
+    })
+
+    // Converts ReadableStream to a NodeJS Stream
+    const nodeStream = Readable.from(response.body)
+    ctx.body = nodeStream
+  })
+
+  return app
+}
