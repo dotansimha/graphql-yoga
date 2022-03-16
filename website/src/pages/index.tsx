f95869dcaec90e167963b0a89b26b730faffc7f8
TuvalSimha <tuval.simha@gmail.com>
2022-03-16 11:09:38 +0200

    test
    header + body
    
    fix
    
    Fix
    
    fix
    
    text fix
    
    buttons
    
    Fix text
    
    fix
    
    fix
    
    Fix root
    
    Fix mobile view

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,55 +1,153 @@
-import { HeroGradient, InfoList } from '@theguild/components'
+import { HeroGradient, HeroIllustration } from '@theguild/components'
 import { handlePushRoute } from '@guild-docs/client'
+import { useMemo } from 'react'
+import { ILink } from '@theguild/components/dist/types/components'
+import styled from '@emotion/styled'
+import src from '@chakra-ui/icon/dist/declarations/src'
+
+const Wrapper = styled.div`
+  @media only screen and (max-width: 500px) {
+    & img {
+      display: none !important;
+    }
+    & div {
+      padding-bottom: 0.1rem;
+    }
+  }
+`
+
+interface featuresOptions {
+  title: string
+  description: string
+  imageSrc: string
+  imageAlt: string
+  flipped?: boolean
+  link?: ILink
+}
+const buttonStyle: React.CSSProperties = {
+  color: 'white',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  width: '144px',
+  height: '40px',
+  borderRadius: '6px',
+  backgroundColor: '#1CC8EE',
+  justifyContent: 'center',
+}
 
 export default function Index() {
+  const ITEMS = useMemo<featuresOptions[]>(
+    () => [
+      {
+        title: 'GraphQL over HTTP compliant',
+        description: 'GraphQL over HTTP specification-compliant server.',
+        imageSrc: '/assets/http.svg',
+        imageAlt: 'Server over HTTP',
+        link: {
+          children: 'Learn more',
+          title: 'Learn more',
+          href: '/docs/quick-start',
+          onClick: (e) => handlePushRoute('/docs/quick-start', e),
+          style: buttonStyle,
+        },
+      },
+      {
+        title: 'Extensible Engines with Envelop Ecosystem',
+        description:
+          'Extend GraphQL Engines capabilities with Envelops powerful plugin ecosystem.',
+        imageSrc: '/assets/ecosystem.svg',
+        imageAlt: 'Ecosystem',
+        flipped: true,
+        link: {
+          children: 'Learn more',
+          title: 'Learn more',
+          href: '/docs/features/envelop-plugins',
+          onClick: (e) => handlePushRoute('/docs/features/envelop-plugins', e),
+          style: buttonStyle,
+        },
+      },
+      {
+        title: 'Subscriptions over HTTP in GraphQL',
+        description:
+          'Subscriptions over HTTP instead of WebSockets via Server Sent Events.',
+        imageSrc: '/assets/subscriptions.svg',
+        imageAlt: 'Subscriptions',
+        link: {
+          children: 'Learn more',
+          title: 'Learn more',
+          href: '/docs/features/subscriptions',
+          onClick: (e) => handlePushRoute('/docs/features/subscriptions', e),
+          style: buttonStyle,
+        },
+      },
+      {
+        title: 'Handle file uploads with GraphQL?',
+        description:
+          'Simply enable uploads and start accepting files for GraphQL mutations.',
+        imageSrc: '/assets/uploads.svg',
+        imageAlt: 'Uploads',
+        flipped: true,
+        link: {
+          children: 'Learn more',
+          title: 'Learn more',
+          href: '/docs/features/file-uploads',
+          onClick: (e) => handlePushRoute('/docs/features/file-uploads', e),
+          style: buttonStyle,
+        },
+      },
+    ],
+    [],
+  )
   return (
     <>
-      <HeroGradient
-        title="GraphQL Yoga"
-        description="Fully-featured GraphQL Server with focus on easy setup, performance and great developer experience"
-        link={[
-          {
-            href: '/docs/quick-start',
-            children: 'Read the Docs',
-            title: 'Read the Yoga Docs',
-            onClick: (e) => handlePushRoute('/docs/quick-start', e),
-          },
-          {
-            href: '/docs/tutorial',
-            children: 'Start the Tutorial',
-            title: 'Start the Tutorial',
-            onClick: (e) => handlePushRoute('/tutorial', e),
-          },
-        ]}
-        // TODO: add it back in when v2 is stable
-        // version={<NPMBadge name="graphql-yoga" />}
-        colors={['#59BDEF', '#ED2E7E']}
-      />
-
-      <InfoList
-        title="Features"
-        items={[
-          {
-            title: 'Spec Compliant',
-            description: 'GraphQL over HTTP specification-compliant server.',
-          },
-          {
-            title: 'Extensible',
-            description:
-              "Extend GraphQL Engine's capabilities with Envelop's powerful plugin ecosystem.",
-          },
-          {
-            title: 'Subscriptions over HTTP',
-            description:
-              'Subscriptions over HTTP instead of WebSockets via Server Sent Events.',
-          },
-          {
-            title: 'Uploads',
-            description:
-              'Simply enable uploads and start accepting files for GraphQL mutations.',
-          },
-        ]}
-      />
+      <Wrapper>
+        <HeroGradient
+          title="Time to Relax with GraphQL Yoga"
+          description={`Fully-featured GraphQL Server with focus on easy setup, performance and great developer experience`}
+          link={[
+            {
+              href: '/docs/quick-start',
+              children: 'Read the docs',
+              title: 'Read the Yoga Docs',
+              onClick: (e) => handlePushRoute('/docs/quick-start', e),
+            },
+            {
+              href: '/docs/tutorial',
+              children: 'Start the Tutorial',
+              title: 'Start the Tutorial',
+              onClick: (e) => handlePushRoute('/tutorial', e),
+              style: {
+                color: '#fff',
+                border: '1px solid #fff',
+                background: 'transparent',
+              },
+            },
+          ]}
+          colors={['#7433FF', '#1CC8EE']}
+          image={{
+            src: '/assets/yogaHome.svg',
+            alt: 'Yoga',
+          }}
+        />
+      </Wrapper>
+      {ITEMS.map((option) => {
+        return (
+          <>
+            <HeroIllustration
+              key={option.title}
+              title={option.title}
+              description={option.description}
+              image={{
+                src: option.imageSrc,
+                alt: option.imageAlt,
+              }}
+              flipped={option.flipped}
+              link={option.link}
+            />
+          </>
+        )
+      })}
     </>
   )
 }
