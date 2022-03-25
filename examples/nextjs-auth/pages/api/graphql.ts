ee2938f11accb317cb20db066209f4111742057d
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-03-25 12:13:33 +0300

    Simplify next-auth example and update next docs (#989)

--- examples/nextjs-auth/pages/api/graphql.ts
+++ examples/nextjs-auth/pages/api/graphql.ts
@@ -1,21 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { createServer } from '@graphql-yoga/node'
-import { NextApiResponse } from 'next'
-import { NextApiHandler, NextApiRequest } from 'next'
+import { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/react'
-import { Session } from 'next-auth'
-import { Readable } from 'stream'
 
 const server = createServer<{
   req: NextApiRequest
   res: NextApiResponse
-  session: Session
 }>({
   cors: false,
   endpoint: '/api/graphql',
-  context: (context) => {
-    console.log(context.session)
-    return context
+  context: async ({ req }) => {
+    return {
+      session: await getSession({ req }),
+    }
   },
   schema: {
     typeDefs: /* GraphQL */ `
@@ -60,20 +57,4 @@ export const config = {
   },
 }
 
-const graphqlHandler: NextApiHandler = async (req, res) => {
-  const session = await getSession({ req })
-  const response = await server.handleIncomingMessage(req, {
-    req,
-    res,
-    session,
-  })
-  for (const [name, value] of response.headers) {
-    res.setHeader(name, value)
-  }
-
-  res.status(response.status)
-  const nodeStream = Readable.from(response.body!)
-  res.send(nodeStream)
-}
-
-export default graphqlHandler
+export default server.requestListener
--- examples/nextjs/pages/api/graphql.ts
+++ examples/nextjs/pages/api/graphql.ts
@@ -1,7 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { createServer } from '@graphql-yoga/node'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-const server = createServer({
+const server = createServer<{
+  req: NextApiRequest
+  res: NextApiResponse
+}>({
   cors: false,
   endpoint: '/api/graphql',
 })
