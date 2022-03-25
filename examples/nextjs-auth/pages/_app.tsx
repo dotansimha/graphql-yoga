60322729a65a9f37444dbc7ef02bf988aaee5a64
Laurin Quast <laurinquast@googlemail.com>
2022-03-25 08:56:42 +0100

    feat: add nextjs auth example (#984)
    * feat: add nextjs auth example
    
    * chore: ignore the engines
    
    * feat: expose session via Query root type

---
+++ examples/nextjs-auth/pages/_app.tsx
@@ -0,0 +1,12 @@
+import { SessionProvider } from 'next-auth/react'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
+}
+
+export default MyApp
