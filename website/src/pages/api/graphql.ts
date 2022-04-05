62be08a5fbcef5fe32f9bd370c9a2fafbb882ec8
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-04-05 17:14:59 +0300

    Update patch-package

--- website/src/pages/api/graphql.ts
+++ website/src/pages/api/graphql.ts
@@ -1,5 +1,8 @@
 import { createServer } from '@graphql-yoga/node'
+import { renderGraphiQL } from '@graphql-yoga/render-graphiql'
 
-const server = createServer()
+const server = createServer({
+  renderGraphiQL,
+})
 
 export default server
