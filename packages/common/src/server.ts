eb09ab8e7e81b073abcaa0a863e4de47cf1e09f1
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-04-05 11:00:59 -0400

    Update packages/common/src/server.ts
    Co-authored-by: Laurin Quast <laurinquast@googlemail.com>

--- packages/common/src/server.ts
+++ packages/common/src/server.ts
@@ -421,7 +421,7 @@ export class YogaServer<
           `Unable to ${request.method} ${requestUrl.pathname}`,
           {
             status: 404,
-            statusText: `Not Found`,
+            statusText: `not found`,
           },
         )
       }
