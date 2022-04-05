76ee74c263bc55f66d1b1aa4e93fdd465b663fc2
Laurin Quast <laurinquast@googlemail.com>
2022-04-05 17:39:56 +0200

    feat: add live query example (#1050)
    * feat: add live query example
    
    * fix: remove unnecessary print
    
    * fix: lockfile

---
+++ examples/live-query/src/main.ts
@@ -0,0 +1,44 @@
+import { createServer } from '@graphql-yoga/node'
+import { useLiveQuery } from '@envelop/live-query'
+import { InMemoryLiveQueryStore } from '@n1ru4l/in-memory-live-query-store'
+import { GraphQLLiveDirective } from '@n1ru4l/graphql-live-query'
+import { astFromDirective } from '@graphql-tools/utils'
+
+const liveQueryStore = new InMemoryLiveQueryStore()
+
+setInterval(() => {
+  const firstElement = greetings.pop()
+  greetings.unshift(firstElement!)
+  liveQueryStore.invalidate('Query.greetings')
+}, 1000).unref()
+
+const greetings = ['Hi', 'Hello', 'Sup']
+
+const server = createServer({
+  context: () => ({ greetings }),
+  schema: {
+    typeDefs: [
+      /* GraphQL */ `
+        type Query {
+          greetings: [String]!
+        }
+      `,
+      astFromDirective(GraphQLLiveDirective),
+    ],
+    resolvers: {
+      Query: {
+        greetings: (_, __, context) => context.greetings,
+      },
+    },
+  },
+  graphiql: {
+    defaultQuery: /* GraphQL */ `
+      query @live {
+        greetings
+      }
+    `,
+  },
+  plugins: [useLiveQuery({ liveQueryStore })],
+})
+
+server.start()
