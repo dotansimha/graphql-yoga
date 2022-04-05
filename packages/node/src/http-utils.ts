8ff292a30bf613493b7ea6dab83857e8cba4d7eb
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-04-05 17:47:14 +0300

    Reduce diff

--- packages/node/src/http-utils.ts
+++ packages/node/src/http-utils.ts
@@ -40,7 +40,7 @@ function getRequestAddressInfo(
       defaultAddressInfo.protocol ||
       'http') as 'http',
     hostname,
-    endpoint: nodeRequest.url ?? '/',
+    endpoint: nodeRequest.url || defaultAddressInfo.endpoint,
     port,
   }
 }
