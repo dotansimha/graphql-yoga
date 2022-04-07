ddabb391174825ecec4938d9673288b1a787353f
Laurin Quast <laurinquast@googlemail.com>
2022-04-07 13:09:02 +0200

    chore: add route lockfile ci action (#1057)
    * chore: add route lockfile ci action
    
    * fix: update + sort lockfile
    
    * chore: error for redirect pointing to nothing
    
    * Fail only on removed/modified lines + comment a diff (#1058)
    
    * Update website-integrity.yml
    
    * Update website-integrity.yml
    
    * Moar
    
    * Use GITHUB_BASE_REF to detect base branch
    
    * test
    
    * 1
    
    * Use github.base_ref context
    
    * Hmm
    
    * Bring back the route
    
    * Publish comment
    
    * always
    
    * Format
    
    * Format
    
    * fix syntax
    
    * try always()
    
    * come on
    
    * multiline OUTPUT
    
    * Update website/next.config.js
    
    * Update route-lockfile.txt
    
    Co-authored-by: Kamil Kisiela <kamil.kisiela@gmail.com>

---
+++ website/scripts/sitemap-ci.ts
@@ -0,0 +1,44 @@
+import { XMLParser } from 'fast-xml-parser'
+import * as fs from 'fs'
+import type { Redirect } from 'next/dist/lib/load-custom-routes'
+import * as path from 'path'
+import config from '../next.config.js'
+
+const sitemapPath = path.join(__dirname, '..', 'public', 'sitemap-0.xml')
+const lockfilePath = path.join(__dirname, '..', 'route-lockfile.txt')
+
+async function main() {
+  const parser = new XMLParser()
+
+  const d = parser.parse(fs.readFileSync(sitemapPath, 'utf-8'))
+
+  const routes: Array<string> = d.urlset.url.map((url: any) =>
+    url.loc.replace(`https://graphql-yoga.com`, ``),
+  )
+
+  const redirectsPointingToNonExistingStuff: Array<Redirect> = []
+
+  const redirects: Redirect[] = config.redirects()
+
+  for (const redirect of redirects) {
+    if (routes.includes(redirect.destination) === false) {
+      redirectsPointingToNonExistingStuff.push(redirect)
+    }
+    routes.push(`${redirect.source} -> ${redirect.destination}`)
+  }
+
+  if (redirectsPointingToNonExistingStuff.length) {
+    console.error(
+      `The following routes do not point to a route:\n\n` +
+        redirectsPointingToNonExistingStuff.map(
+          (redirect) => `- "${redirect.source}" -> "${redirect.destination}"`,
+        ) +
+        `\n`,
+    )
+    throw new Error('Redirect pointing to nothing.')
+  }
+
+  fs.writeFileSync(lockfilePath, routes.sort().join(`\n`) + `\n`)
+}
+
+main()
