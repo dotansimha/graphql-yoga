d81298c4f234f1d2aad17c4139835b45e8aaa8d7
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-04-13 15:35:53 +0300

    Do not trigger polyfills

--- packages/common/src/encodeString.ts
+++ packages/common/src/encodeString.ts
@@ -1,7 +1,7 @@
 let encodeString: (str: string) => Uint8Array
 if (globalThis.Buffer) {
   encodeString = function encodeStringWithBuffer(str: string) {
-    return Buffer.from(str, 'utf8')
+    return globalThis.Buffer.from(str, 'utf8')
   }
 } else {
   const textEncoder = new TextEncoder()
