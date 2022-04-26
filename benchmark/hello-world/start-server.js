25001fcd761bd028887236975800e39c906900d1
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-04-26 16:43:10 +0300

    Try rootValue in benchmarks

--- benchmark/hello-world/start-server.js
+++ benchmark/hello-world/start-server.js
@@ -1,9 +1,25 @@
-const { createServer } = require('@graphql-yoga/node')
+const { createServer, useSchema } = require('@graphql-yoga/node')
+const { buildSchema, execute } = require('graphql')
 
 const server = createServer({
   logging: false,
   hostname: '127.0.0.1',
-  healthCheckPath: false,
+  plugins: [
+    useSchema(buildSchema('type Query { greetings: String! }')),
+    {
+      onExecute({ setExecuteFn }) {
+        setExecuteFn((args) =>
+          execute({
+            ...args,
+            rootValue: {
+              greetings:
+                'This is the `greetings` field of the root `Query` type',
+            },
+          }),
+        )
+      },
+    },
+  ],
 })
 
 server.start()
