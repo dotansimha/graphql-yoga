7e9733794748afd07267b8e8b22f49b43cbddc3c
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-05-02 20:00:59 +0300

    Fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,31 +1,28 @@
 const { resolve } = require('path')
+const { pathsToModuleNameMapper } = require('ts-jest')
 const CI = !!process.env.CI
 
 const ROOT_DIR = __dirname
 const TSCONFIG = resolve(ROOT_DIR, 'tsconfig.json')
 const tsconfig = require(TSCONFIG)
 
+process.env.LC_ALL = 'en_US'
+
 module.exports = {
   testEnvironment: 'node',
   rootDir: ROOT_DIR,
   restoreMocks: true,
   reporters: ['default'],
-  // TODO: get tests for `examples`
   modulePathIgnorePatterns: ['dist'],
-  moduleNameMapper: {
-    '@graphql-yoga/common': '<rootDir>/packages/common/src/index.ts',
-    '@graphql-yoga/subscription':
-      '<rootDir>/packages/subscription/src/index.ts',
-    '@graphql-yoga/node': '<rootDir>/packages/node/src/index.ts',
-    '@graphql-yoga/render-graphiql':
-      '<rootDir>/packages/render-graphiql/src/index.ts',
-    'graphql-yoga': '<rootDir>/packages/graphql-yoga/src/index.ts',
-  },
-  collectCoverage: true,
+  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
+    prefix: `${ROOT_DIR}/`,
+  }),
+  collectCoverage: false,
   cacheDirectory: resolve(ROOT_DIR, `${CI ? '' : 'node_modules/'}.cache/jest`),
   testMatch: [
     '**/?(*.)+(spec|test).[jt]s?(x)',
     '!**/examples/cloudflare-modules/**',
     '!**/examples/node-esm/**',
   ],
+  extensionsToTreatAsEsm: ['.ts'],
 }
