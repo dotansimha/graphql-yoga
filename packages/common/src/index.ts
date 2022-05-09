1ec580f45de7ff965ffdbff1c39aa196ef9167f5
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-05-09 18:48:39 +0300

    Fix TS errors

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -4,3 +4,10 @@ export * from './server'
 
 export * from '@envelop/core'
 export * from '@graphql-yoga/subscription'
+export { CORSOptions } from './plugins/useCORS'
+export {
+  GraphiQLOptions,
+  shouldRenderGraphiQL,
+  renderGraphiQL,
+} from './plugins/useGraphiQL'
+export { GraphQLYogaError } from './GraphQLYogaError'
