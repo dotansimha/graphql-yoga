c08cac34aba664d717a92cabfff95ea2cdd950ab
JYC <jycouet@gmail.com>
2022-06-06 12:20:33 +0200

    Adding tests on SvelteKit (#1220)
    * :white_check_mark: NEW: Tests on sveltekit
    
    * :white_check_mark: UPDATE: tests & ci for sveltekit
    
    * :ok_hand: IMPROVE: tests on SvelteKit
    
    * :ok_hand: FIX: tests
    
    * Single for all
    
    * Remove extra test workflow
    
    * Cleanup
    
    * Do not run tests in Node 14
    
    * Fix Typechecking
    
    * :facepalm:
    
    * Go
    
    * Go
    
    * ..
    
    Co-authored-by: Arda TANRIKULU <ardatanrikulu@gmail.com>

--- examples/sveltekit/src/routes/api/graphql.ts
+++ examples/sveltekit/src/routes/api/graphql.ts
@@ -1,6 +1,6 @@
 import { useGraphQlJit } from '@envelop/graphql-jit';
 import { createServer } from '@graphql-yoga/common';
-import type { RequestEvent } from '@sveltejs/kit/types/hooks';
+import type { RequestEvent } from '@sveltejs/kit';
 
 const yogaApp = createServer<RequestEvent>({
 	logging: false,
@@ -21,7 +21,10 @@ const yogaApp = createServer<RequestEvent>({
 		// other plugins: https://www.envelop.dev/plugins
 	],
 	graphiql: {
-		endpoint: '/api/graphql'
+		endpoint: '/api/graphql',
+		defaultQuery: `query Hello {
+	hello
+}`
 	}
 });
 
--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,12 @@ const tsconfig = require(TSCONFIG)
 
 process.env.LC_ALL = 'en_US'
 
+const testMatch = ['**/?(*.)+(spec|test).[jt]s?(x)', '!**/examples/node-esm/**']
+
+if (parseInt(process.versions.node.split('.')[0]) <= 14) {
+  testMatch.push('!**/examples/sveltekit/**')
+}
+
 module.exports = {
   testEnvironment: 'node',
   rootDir: ROOT_DIR,
@@ -22,6 +28,6 @@ module.exports = {
   },
   collectCoverage: false,
   cacheDirectory: resolve(ROOT_DIR, `${CI ? '' : 'node_modules/'}.cache/jest`),
-  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)', '!**/examples/node-esm/**'],
+  testMatch,
   extensionsToTreatAsEsm: ['.ts'],
 }
