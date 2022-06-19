6127b94dfe54185a6a1a45ecfd13307cc9ccd260
Laurin Quast <laurinquast@googlemail.com>
2022-06-19 11:03:35 +0200

    chore: use pure js library for minifying html (#1302)

--- packages/common/scripts/generate-graphiql-html.js
+++ packages/common/scripts/generate-graphiql-html.js
@@ -1,31 +1,35 @@
-const minifyHtml = require('@minify-html/js')
+const { minify } = require('html-minifier-terser')
 const { readFileSync, writeFileSync } = require('fs')
 const { join } = require('path')
 const fs = require('fs')
 
-const graphiqlVersion = JSON.parse(
-  fs.readFileSync(
-    join(__dirname, '..', '..', 'graphiql', 'package.json'),
-    'utf-8',
-  ),
-).version
-
-const cfg = minifyHtml.createConfiguration({
-  do_not_minify_doctype: true,
-  keep_spaces_between_attributes: true,
-  ensure_spec_compliant_unquoted_attribute_values: true,
-  minify_js: true,
-})
-const minified = minifyHtml
-  .minify(
-    readFileSync(
-      join(__dirname, '..', 'src', 'graphiql.html'),
+async function main() {
+  const graphiqlVersion = JSON.parse(
+    fs.readFileSync(
+      join(__dirname, '..', '..', 'graphiql', 'package.json'),
       'utf-8',
-    ).replace(/__GRAPHIQL_VERSION__/g, graphiqlVersion),
-    cfg,
+    ),
+  ).version
+
+  const minified = (
+    await minify(
+      readFileSync(
+        join(__dirname, '..', 'src', 'graphiql.html'),
+        'utf-8',
+      ).replace(/__GRAPHIQL_VERSION__/g, graphiqlVersion),
+      {
+        minifyJS: true,
+        useShortDoctype: false,
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+      },
+    )
+  ).toString('utf-8')
+
+  writeFileSync(
+    join(__dirname, '../src/graphiqlHTML.ts'),
+    `export default ${JSON.stringify(minified)}`,
   )
-  .toString('utf-8')
-writeFileSync(
-  join(__dirname, '../src/graphiqlHTML.ts'),
-  `export default ${JSON.stringify(minified)}`,
-)
+}
+
+main()
