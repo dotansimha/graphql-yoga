3985e1d7d8494d22e31252a64c7417abad3e158d
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-06-22 12:35:11 +0300

    Ignore wasm files

--- resolver.js
+++ resolver.js
@@ -3,7 +3,13 @@ module.exports = (path, options) => {
    * Jest does not like .js extensions when not running Jest in EXPERIMENTAL AND TOTALLY UNSTABLE mode.
    * We rewrite it so we can run the tests.
    */
-  if (path.startsWith('.') && path.endsWith('.js')) {
+  if (
+    path.startsWith('.') &&
+    path.endsWith('.js') &&
+    // Wasm imports should remain as is
+    // Kind of related -> https://github.com/nodejs/undici/issues/1087
+    !path.endsWith('.wasm.js')
+  ) {
     path = path.replace(/\.js$/, '')
   }
 
