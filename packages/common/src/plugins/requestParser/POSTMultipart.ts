4b1b3a768156e1587d3accff7b05da6907bef06f
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-06-28 19:11:36 +0300

    Move try/catch around formData method

--- packages/common/src/plugins/requestParser/POSTMultipart.ts
+++ packages/common/src/plugins/requestParser/POSTMultipart.ts
@@ -1,3 +1,4 @@
+import { createGraphQLError } from '@graphql-tools/utils/typings/errors.js'
 import { dset } from 'dset'
 import { GraphQLParams, GraphQLYogaError } from '../../types.js'
 import { isContentTypeMatch } from './utils.js'
@@ -12,39 +13,43 @@ export function isPOSTMultipartRequest(request: Request): boolean {
 export async function parsePOSTMultipartRequest(
   request: Request,
 ): Promise<GraphQLParams> {
+  let requestBody: FormData
   try {
-    const requestBody = await request.formData()
-    const operationsStr = requestBody.get('operations')?.toString() || '{}'
-    const operations = JSON.parse(operationsStr)
-    const mapStr = requestBody.get('map')?.toString() || '{}'
-    const map = JSON.parse(mapStr)
-    for (const fileIndex in map) {
-      const file = requestBody.get(fileIndex)
-      const keys = map[fileIndex]
-      for (const key of keys) {
-        dset(operations, key, file)
-      }
-    }
-
-    return {
-      operationName: operations.operationName,
-      query: operations.query,
-      variables: operations.variables,
-      extensions: operations.extensions,
-    }
-  } catch (e: any) {
+    requestBody = await request.formData()
+  } catch (e: unknown) {
     // Trick for cross-undici-fetch errors on Node.js
     // TODO: This needs a better solution
     if (
       e instanceof Error &&
       e.message.startsWith('File size limit exceeded: ')
     ) {
-      throw new GraphQLYogaError(e.message, {
-        http: {
-          status: 413,
+      throw createGraphQLError(e.message, {
+        extensions: {
+          http: {
+            status: 413,
+          },
         },
       })
     }
     throw e
+  }
+
+  const operationsStr = requestBody.get('operations')?.toString() || '{}'
+  const operations = JSON.parse(operationsStr)
+  const mapStr = requestBody.get('map')?.toString() || '{}'
+  const map = JSON.parse(mapStr)
+  for (const fileIndex in map) {
+    const file = requestBody.get(fileIndex)
+    const keys = map[fileIndex]
+    for (const key of keys) {
+      dset(operations, key, file)
+    }
+  }
+
+  return {
+    operationName: operations.operationName,
+    query: operations.query,
+    variables: operations.variables,
+    extensions: operations.extensions,
   }
 }
