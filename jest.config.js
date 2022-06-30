48696e35cd25b4a2319c4452bca4ace2b9ea478e
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-06-30 18:45:58 +0300

    Cleanup

--- jest.config.js
+++ jest.config.js
@@ -34,7 +34,5 @@ module.exports = {
   collectCoverage: false,
   cacheDirectory: resolve(ROOT_DIR, `${CI ? '' : 'node_modules/'}.cache/jest`),
   testMatch,
-  // TODO: Remove once we drop Node 12 support
-  transformIgnorePatterns: ['node_modules/(?!fastify)'],
   resolver: 'bob-the-bundler/jest-resolver.js',
 }
