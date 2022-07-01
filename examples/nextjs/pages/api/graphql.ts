a771f1359dd45c6a83e49b7285d64dd8ab781509
Laurin Quast <laurinquast@googlemail.com>
2022-07-01 08:05:19 +0200

    docs: update examples and website on Next.js with disabled body parsing

--- examples/nextjs/pages/api/graphql.ts
+++ examples/nextjs/pages/api/graphql.ts
@@ -1,10 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { createServer } from '@graphql-yoga/node'
-import { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-const server = createServer<{
+export const config = {
+  api: {
+    // Disable body parsing (required for file uploads)
+    bodyParser: false,
+  },
+}
+
+export default createServer<{
   req: NextApiRequest
   res: NextApiResponse
 }>()
-
-export default server
