8508407e8de25b91a0da9f2dc654c4e46e2efac7
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-07-26 17:13:59 +0300

    Fix CF E2E

--- examples/service-worker/src/index.ts
+++ examples/service-worker/src/index.ts
@@ -1,5 +1,10 @@
 import { createYoga } from 'graphql-yoga'
 
-const yoga = createYoga()
+// We can define GraphQL Route dynamically using env vars.
+declare var GRAPHQL_ROUTE: string
+
+const yoga = createYoga({
+  graphqlEndpoint: GRAPHQL_ROUTE || '/graphql',
+})
 
 self.addEventListener('fetch', yoga)
