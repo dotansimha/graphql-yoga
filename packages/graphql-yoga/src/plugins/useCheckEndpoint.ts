8713f29fb8c49ce9f612e1d24c499f3c6cfe3e8d
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-07-26 16:40:08 +0300

    Add an ugly 404 page

--- packages/graphql-yoga/src/plugins/useCheckEndpoint.ts
+++ packages/graphql-yoga/src/plugins/useCheckEndpoint.ts
@@ -7,21 +7,51 @@ export function useCheckEndpoint(graphqlEndpoint: string): Plugin {
       const { pathname: requestPath } = new URL(request.url)
       if (requestPath !== graphqlEndpoint) {
         const errorMessage = `
+        <html>
+          <head>
+            <title>GraphQL Yoga - 404 Not Found</title>
+            <style>
+              body {
+                font-family: monospace;
+                font-size: 12px;
+                margin: 10px auto;
+                text-align: center;
+              }
+              code {
+                background: #d5d5d5;
+                color: brown;
+              }
+              textarea {
+                width: 450px;
+              }
+            </style>
+          </head>
+          <body>
             <p>Unable to <code>${request.method}</code> <code>${requestPath}</code>
             <hr>
-            <strong>GraphQL Endpoint is set to <code>${graphqlEndpoint}</code> now.</strong>
-            <strong>So if you expect it to be <code>${requestPath}</code> please add <code>graphqlEndpoint: '${requestPath}'</code> GraphQL Yoga configuration like below.</strong>
-            <code>
-              createServer({
+            <p>GraphQL Endpoint is set to <code>${graphqlEndpoint}</code> now.<p>
+            <p>
+              So if you expect it to be <code>${requestPath}</code>
+              please add <code>graphqlEndpoint: '${requestPath}'</code> to GraphQL Yoga configuration like below;
+            </p>
+            <textarea readonly rows="10">
+              import { createYoga } from 'graphql-yoga';
+              import { schema } from './schema.js';
+              const yoga = createYoga({
                 schema,
                 graphqlEndpoint: '${requestPath}',
               })
-            </code>
+            </textarea>
+          </body>
+        </html>
           `
         endResponse(
           new fetchAPI.Response(errorMessage, {
             status: 404,
             statusText: 'Not Found',
+            headers: {
+              'Content-Type': 'text/html',
+            },
           }),
         )
       }
