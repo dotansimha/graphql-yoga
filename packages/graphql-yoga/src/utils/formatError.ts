1b1cb0337d41a0b8384fa817385d026c75fa9cc2
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-07-28 19:48:57 +0300

    Fix tests

--- packages/graphql-yoga/src/utils/formatError.ts
+++ packages/graphql-yoga/src/utils/formatError.ts
@@ -1,8 +1,31 @@
 import { FormatErrorHandler } from '@envelop/core'
+import { createGraphQLError } from '@graphql-tools/utils'
 import { GraphQLError } from 'graphql'
 
 export const formatError: FormatErrorHandler = (err, message, isDev) => {
   if (err instanceof GraphQLError) {
+    if (err.originalError) {
+      if (err.originalError.name === 'GraphQLError') {
+        return err
+      }
+      // Original error should be removed
+      const extensions = {
+        ...err.extensions,
+      }
+      if (isDev) {
+        extensions.originalError = {
+          message: err.originalError.message,
+          stack: err.originalError.stack,
+        }
+      }
+      return createGraphQLError(message, {
+        nodes: err.nodes,
+        source: err.source,
+        positions: err.positions,
+        path: err.path,
+        extensions,
+      })
+    }
     return err
   }
   return new GraphQLError(message)
