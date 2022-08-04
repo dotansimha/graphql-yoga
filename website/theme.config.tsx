5b066ebbb3970ecf8d0d2b36067a63d88d9e4103
TuvalSimha <tuval.simha@gmail.com>
2022-08-04 14:32:37 +0300

    bootstrap
    fix menu

---
+++ website/theme.config.tsx
@@ -0,0 +1,44 @@
+import { YogaLogo } from '@theguild/components'
+// @ts-ignore
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const SITE_NAME = 'GraphQL Yoga'
+
+const config: DocsThemeConfig = {
+  titleSuffix: ` – ${SITE_NAME}`,
+  projectLink: 'https://github.com/Urigo/graphql-modules', // GitHub link in the navbar
+  docsRepositoryBase:
+    'https://github.com/Urigo/graphql-modules/tree/master/website/src/pages', // base URL for the docs repository
+  nextLinks: true,
+  prevLinks: true,
+  search: false,
+  floatTOC: true,
+  darkMode: true,
+  footer: false,
+  footerEditLink: 'Edit this page on GitHub',
+  logo: (
+    <>
+      <YogaLogo className="mr-1.5 h-9 w-9" />
+      <div>
+        <h1 className="md:text-md text-sm font-medium">{SITE_NAME}</h1>
+        <h2 className="hidden text-xs sm:!block">
+          Fully-featured, simple to set up, performant and extendable GraphQL
+          JavaScript server Home
+        </h2>
+      </div>
+    </>
+  ),
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content={`${SITE_NAME}: documentation`} />
+      <meta name="og:title" content={`${SITE_NAME}: documentation`} />
+    </>
+  ),
+  gitTimestamp: 'Last updated on',
+  defaultMenuCollapsed: true,
+  feedbackLink: 'Question? Give us feedback →',
+  feedbackLabels: 'kind/docs',
+}
+
+export default config
