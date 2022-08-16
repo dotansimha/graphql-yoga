c6dcc0f67efac391deecb107fab1de415efc0242
Laurin Quast <laurinquast@googlemail.com>
2022-08-16 11:44:17 +0200

    fix implementation

--- packages/plugins/apq/__tests__/apq.spec.ts
+++ packages/plugins/apq/__tests__/apq.spec.ts
@@ -1,6 +1,4 @@
 import { createYoga, createSchema } from 'graphql-yoga'
-
-import request from 'supertest'
 import { createInMemoryAPQStore, useAPQ } from '@graphql-yoga/plugin-apq'
 
 const schema = createSchema({
@@ -12,7 +10,7 @@ const schema = createSchema({
 })
 
 describe('Automatic Persisted Queries', () => {
-  it('should return not found error if persisted query is missing', async () => {
+  it('sends not found error to client if persisted query is missing', async () => {
     const store = createInMemoryAPQStore()
     const yoga = createYoga({
       plugins: [
@@ -22,9 +20,12 @@ describe('Automatic Persisted Queries', () => {
       ],
       schema,
     })
-    const response = await request(yoga)
-      .post('/graphql')
-      .send({
+    const response = await yoga.fetch('http://yoga/graphql', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
         extensions: {
           persistedQuery: {
             version: 1,
@@ -32,13 +33,14 @@ describe('Automatic Persisted Queries', () => {
               'ecf4edb46db40b5132295c0291d62fb65d6759a9eedfa4d5d612dd5ec54a6b38',
           },
         },
-      })
+      }),
+    })
 
-    const body = JSON.parse(response.text)
+    const body = JSON.parse(await response.text())
     expect(body.errors).toBeDefined()
     expect(body.errors[0].message).toBe('PersistedQueryNotFound')
   })
-  it('should load the persisted query when stored', async () => {
+  it('uses a stored persisted query', async () => {
     const store = createInMemoryAPQStore()
     const yoga = createYoga({
       plugins: [
@@ -54,19 +56,23 @@ describe('Automatic Persisted Queries', () => {
         'ecf4edb46db40b5132295c0291d62fb65d6759a9eedfa4d5d612dd5ec54a6b38',
     }
     store.set(persistedQueryEntry.sha256Hash, '{__typename}')
-    const response = await request(yoga)
-      .post('/graphql')
-      .send({
+    const response = await yoga.fetch('http://yoga/graphql', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
         extensions: {
           persistedQuery: persistedQueryEntry,
         },
-      })
+      }),
+    })
 
-    const body = JSON.parse(response.text)
+    const body = JSON.parse(await response.text())
     expect(body.errors).toBeUndefined()
     expect(body.data.__typename).toBe('Query')
   })
-  it('should save the persisted query', async () => {
+  it('saves a persisted query', async () => {
     const store = createInMemoryAPQStore()
     const yoga = createYoga({
       plugins: [
@@ -83,19 +89,24 @@ describe('Automatic Persisted Queries', () => {
         'ecf4edb46db40b5132295c0291d62fb65d6759a9eedfa4d5d612dd5ec54a6b38',
     }
     const query = `{__typename}`
-    const response = await request(yoga)
-      .post('/graphql')
-      .send({
+
+    const response = await yoga.fetch('http://yoga/graphql', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
         query,
         extensions: {
           persistedQuery: persistedQueryEntry,
         },
-      })
+      }),
+    })
 
     const entry = store.get(persistedQueryEntry.sha256Hash)
     expect(entry).toBe(query)
 
-    const body = JSON.parse(response.text)
+    const body = JSON.parse(await response.text())
     expect(body.errors).toBeUndefined()
     expect(body.data.__typename).toBe('Query')
   })
@@ -110,9 +121,12 @@ describe('Automatic Persisted Queries', () => {
       schema,
     })
     const query = `{__typename}`
-    const response = await request(yoga)
-      .post('/graphql')
-      .send({
+    const response = await yoga.fetch('http://yoga/graphql', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
         query,
         extensions: {
           persistedQuery: {
@@ -120,16 +134,12 @@ describe('Automatic Persisted Queries', () => {
             sha256Hash: 'leoeoel',
           },
         },
-      })
+      }),
+    })
+
     expect(response.status).toEqual(500)
-    expect(JSON.parse(response.text)).toMatchInlineSnapshot(`
-      Object {
-        "errors": Array [
-          Object {
-            "message": "PersistedQueryMismatch",
-          },
-        ],
-      }
-    `)
+    expect(JSON.parse(await response.text())).toEqual({
+      errors: [{ message: 'PersistedQueryMismatch' }],
+    })
   })
 })
