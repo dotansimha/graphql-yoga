49200a48396f6330493196e28b4e2d935f09775f
enisdenjo <badurinadenis@gmail.com>
2022-08-18 11:02:50 +0200

    micro reorg

--- packages/plugins/inline-trace/__tests__/inline-trace.spec.ts
+++ packages/plugins/inline-trace/__tests__/inline-trace.spec.ts
@@ -2,7 +2,6 @@ import request from 'supertest'
 import { createYoga, createSchema } from 'graphql-yoga'
 import { useInlineTrace } from '../src'
 import { Trace } from 'apollo-reporting-protobuf'
-import { GraphQLError } from 'graphql'
 
 describe('Inline Trace', () => {
   const yoga = createYoga({
@@ -42,6 +41,46 @@ describe('Inline Trace', () => {
     expect(response.body?.extensions?.ftv1).toBeDefined()
   })
 
+  function expectTrace(trace: Trace) {
+    expect(trace.startTime).toBeDefined()
+    expect(typeof trace.startTime?.seconds).toBe('number')
+    expect(typeof trace.startTime?.nanos).toBe('number')
+
+    expect(typeof trace.durationNs).toBe('number')
+
+    expect(trace.endTime).toBeDefined()
+    expect(typeof trace.endTime?.seconds).toBe('number')
+    expect(typeof trace.endTime?.nanos).toBe('number')
+
+    expect(
+      addSecondsAndNanos(trace.startTime!.seconds!, trace.startTime!.nanos!),
+    ).toBeLessThan(
+      addSecondsAndNanos(trace.endTime!.seconds!, trace.endTime!.nanos!),
+    )
+
+    expect(typeof trace.fieldExecutionWeight).toBe('number')
+
+    expect(trace.root).toBeDefined()
+    expect(trace.root?.child).toBeInstanceOf(Array)
+  }
+
+  function expectTraceNode(
+    node: Trace.INode | null | undefined,
+    field: string,
+    type: string,
+  ) {
+    expect(node).toBeDefined()
+
+    expect(node!.responseName).toBe(field)
+    expect(node!.type).toBe(type)
+    expect(node!.parentType).toBe('Query')
+
+    expect(typeof node!.startTime).toBe('number')
+    expect(typeof node!.endTime).toBe('number')
+
+    expect(node!.startTime!).toBeLessThan(node!.endTime!)
+  }
+
   it('should contain valid proto tracing details on success', async () => {
     const response = await request(yoga)
       .post('/graphql')
@@ -65,6 +104,20 @@ describe('Inline Trace', () => {
     const hello = trace.root!.child![0]
     expectTraceNode(hello, 'hello', 'String!')
   })
+
+  function expectTraceNodeError(node: Trace.INode | null | undefined) {
+    expect(node).toBeDefined()
+    expect(node!.error).toBeDefined()
+    const error = node!.error!
+    expect(error).toBeInstanceOf(Array)
+    expect(typeof error[0].message).toBe('string')
+    expect(typeof error[0].location).toBeDefined()
+    expect(typeof error[0].location?.[0].line).toBe('number')
+    expect(typeof error[0].location?.[0].column).toBe('number')
+    expect(() => {
+      JSON.parse(error[0].json!)
+    }).not.toThrow()
+  }
 
   it('should contain valid proto tracing details on parse fail', async () => {
     const response = await request(yoga)
@@ -132,60 +185,6 @@ describe('Inline Trace', () => {
     expectTraceNodeError(boom)
   })
 })
-
-function expectTrace(trace: Trace) {
-  expect(trace.startTime).toBeDefined()
-  expect(typeof trace.startTime?.seconds).toBe('number')
-  expect(typeof trace.startTime?.nanos).toBe('number')
-
-  expect(typeof trace.durationNs).toBe('number')
-
-  expect(trace.endTime).toBeDefined()
-  expect(typeof trace.endTime?.seconds).toBe('number')
-  expect(typeof trace.endTime?.nanos).toBe('number')
-
-  expect(
-    addSecondsAndNanos(trace.startTime!.seconds!, trace.startTime!.nanos!),
-  ).toBeLessThan(
-    addSecondsAndNanos(trace.endTime!.seconds!, trace.endTime!.nanos!),
-  )
-
-  expect(typeof trace.fieldExecutionWeight).toBe('number')
-
-  expect(trace.root).toBeDefined()
-  expect(trace.root?.child).toBeInstanceOf(Array)
-}
-
-function expectTraceNode(
-  node: Trace.INode | null | undefined,
-  field: string,
-  type: string,
-) {
-  expect(node).toBeDefined()
-
-  expect(node!.responseName).toBe(field)
-  expect(node!.type).toBe(type)
-  expect(node!.parentType).toBe('Query')
-
-  expect(typeof node!.startTime).toBe('number')
-  expect(typeof node!.endTime).toBe('number')
-
-  expect(node!.startTime!).toBeLessThan(node!.endTime!)
-}
-
-function expectTraceNodeError(node: Trace.INode | null | undefined) {
-  expect(node).toBeDefined()
-  expect(node!.error).toBeDefined()
-  const error = node!.error!
-  expect(error).toBeInstanceOf(Array)
-  expect(typeof error[0].message).toBe('string')
-  expect(typeof error[0].location).toBeDefined()
-  expect(typeof error[0].location?.[0].line).toBe('number')
-  expect(typeof error[0].location?.[0].column).toBe('number')
-  expect(() => {
-    JSON.parse(error[0].json!)
-  }).not.toThrow()
-}
 
 function addSecondsAndNanos(seconds: number, nanos: number): number {
   return seconds + nanos / 1e9
