3973d03944fc5d59006c2f644e8457cece8ec81b
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-08-23 13:53:46 +0200

    temp

--- website/src/pages/index-page.tsx
+++ website/src/pages/index-page.tsx
@@ -1,7 +1,3 @@
----
-title: Home
----
-
 import { HeroGradient, HeroIllustration } from '@theguild/components'
 import { handlePushRoute } from 'guild-docs'
 
@@ -15,7 +11,6 @@ export const ITEMS = [
       children: 'Learn more',
       title: 'Learn more',
       href: '/docs/quick-start',
-      onClick: (e) => handlePushRoute('/docs/quick-start', e),
     },
   },
   {
@@ -29,7 +24,6 @@ export const ITEMS = [
       children: 'Learn more',
       title: 'Learn more',
       href: '/docs/features/envelop-plugins',
-      onClick: (e) => handlePushRoute('/docs/features/envelop-plugins', e),
     },
   },
   {
@@ -42,7 +36,6 @@ export const ITEMS = [
       children: 'Learn more',
       title: 'Learn more',
       href: '/docs/features/subscriptions',
-      onClick: (e) => handlePushRoute('/docs/features/subscriptions', e),
     },
   },
   {
@@ -56,51 +49,56 @@ export const ITEMS = [
       children: 'Learn more',
       title: 'Learn more',
       href: '/docs/features/file-uploads',
-      onClick: (e) => handlePushRoute('/docs/features/file-uploads', e),
     },
   },
 ]
 
-<HeroGradient
-  title="Time to Relax with GraphQL Yoga"
-  description="The fully-featured GraphQL Server with focus on easy setup, performance and great developer experience."
-  link={[
-    {
-      href: '/docs/quick-start',
-      children: 'Read the docs',
-      title: 'Read the Yoga Docs',
-      onClick: (e) => handlePushRoute('/docs/quick-start', e),
-    },
-    {
-      href: '/docs/tutorial',
-      children: 'Start the Tutorial',
-      title: 'Start the Tutorial',
-      onClick: (e) => handlePushRoute('/tutorial', e),
-      style: {
-        color: '#fff',
-        border: '1px solid #fff',
-        background: 'transparent',
-      },
-    },
-  ]}
-  colors={['#7433ff', '#1cc8ee']}
-  image={{
-    src: '/assets/yogaHome.svg',
-    alt: 'Yoga',
-  }}
-/>
+export default function IndexPage() {
+  return (
+    <>
+      <HeroGradient
+        title="Time to Relax with GraphQL Yoga"
+        description="The fully-featured GraphQL Server with focus on easy setup, performance and great developer experience."
+        link={[
+          {
+            href: '/docs/quick-start',
+            children: 'Read the docs',
+            title: 'Read the Yoga Docs',
+            onClick: (e) => handlePushRoute('/docs/quick-start', e),
+          },
+          {
+            href: '/docs/tutorial',
+            children: 'Start the Tutorial',
+            title: 'Start the Tutorial',
+            onClick: (e) => handlePushRoute('/tutorial', e),
+            style: {
+              color: '#fff',
+              border: '1px solid #fff',
+              background: 'transparent',
+            },
+          },
+        ]}
+        colors={['#7433ff', '#1cc8ee']}
+        image={{
+          src: '/assets/yogaHome.svg',
+          alt: 'Yoga',
+        }}
+      />
 
-{ITEMS.map((option) => (
+      {ITEMS.map((option) => (
 
-<HeroIllustration
-  key={option.title}
-  title={option.title}
-  description={option.description}
-  image={{
-    src: option.imageSrc,
-    alt: option.imageAlt,
-  }}
-  flipped={option.flipped}
-  link={option.link}
-/>
-))}
+        <HeroIllustration
+          key={option.title}
+          title={option.title}
+          description={option.description}
+          image={{
+            src: option.imageSrc,
+            alt: option.imageAlt,
+          }}
+          flipped={option.flipped}
+          link={option.link}
+        />
+      ))}
+    </>
+  )
+}
