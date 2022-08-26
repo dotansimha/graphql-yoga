54ca781ac4085060af94c2cac70e4aaeecdedb86
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-08-26 13:36:58 +0300

    Update

--- examples/deno/index.ts
+++ examples/deno/index.ts
@@ -1,9 +1,9 @@
 import { serve } from 'https://deno.land/std@0.153.0/http/server.ts'
-import { createServer } from 'https://cdn.skypack.dev/@graphql-yoga/common'
+import { createServer } from 'https://cdn.skypack.dev/@graphql-yoga/common?dts'
 
 const graphQLServer = createServer()
 
-serve(graphQLServer.handleRequest, {
+serve(graphQLServer, {
   port: 4000,
 })
 
