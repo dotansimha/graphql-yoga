313e6f3036a1ddeb7e6299925a49bb7f0c2d2eb6
Paul <paul.cowan@cutting.scot>
2022-08-26 11:48:59 +0100

    upgrade and fix Deno example (#1667)
    * upgrade and fix Deno example
    
    * Update
    
    * fix(common): accept the server context if Yoga instance is used as a Request handler
    
    Co-authored-by: Arda TANRIKULU <ardatanrikulu@gmail.com>

--- examples/deno/index.ts
+++ examples/deno/index.ts
@@ -1,10 +1,10 @@
-import { serve } from 'https://deno.land/std@0.117.0/http/server.ts'
-import { createServer } from 'https://cdn.skypack.dev/@graphql-yoga/common'
+import { serve } from 'https://deno.land/std@0.153.0/http/server.ts'
+import { createServer } from 'https://cdn.skypack.dev/@graphql-yoga/common?dts'
 
 const graphQLServer = createServer()
 
 serve(graphQLServer, {
-  addr: ':4000',
+  port: 4000,
 })
 
 console.log('Server is running on http://localhost:4000/graphql')
