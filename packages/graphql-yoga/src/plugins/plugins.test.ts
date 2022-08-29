ada2a21538cb1b1e6b2ff4bad2f9a5f39320103e
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2022-08-29 11:07:11 -0400

    make build pass

--- packages/graphql-yoga/src/plugins/plugins.test.ts
+++ packages/graphql-yoga/src/plugins/plugins.test.ts
@@ -1,7 +1,7 @@
 import { AfterValidateHook } from '@envelop/core'
 import { GraphQLError } from 'graphql'
-import { Plugin } from '../src/plugins/types'
-import { createServer } from '../src/server'
+import { Plugin } from './types'
+import { createYoga } from '../server'
 
 describe('Yoga Plugins', () => {
   it(`should respect Envelop's OnPluginInit's addPlugin`, async () => {
@@ -20,7 +20,7 @@ describe('Yoga Plugins', () => {
         addPlugin(testPlugin)
       },
     }
-    const yoga = createServer({
+    const yoga = createYoga({
       plugins: [testPluginToAdd],
     })
     const response = await yoga.fetch('http://localhost:3000/graphql', {
