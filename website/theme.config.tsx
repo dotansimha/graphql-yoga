1de448e8561e6eaa9cebad0d9b80fbe47afaf2a9
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-09-13 19:34:00 +0300

    update components

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -1,6 +1,13 @@
-import { YogaLogo, DocsThemeConfig } from '@theguild/components'
-// @ts-ignore -- TODO: @laurin why I get TS2307: Cannot find module 'guild-docs/giscus' or its corresponding type declarations.
-import type { Giscus } from 'guild-docs/giscus'
+/* eslint sort-keys: error */
+import {
+  YogaLogo,
+  DocsThemeConfig,
+  FooterExtended,
+  Header,
+  Navbar,
+} from '@theguild/components'
+// @ts-ignore -- TODO: @laurin why I get TS2307: Cannot find module '@theguild/components/giscus' or its corresponding type declarations.
+import type { Giscus } from '@theguild/components/giscus'
 import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router'
 
@@ -8,22 +15,26 @@ const SITE_NAME = 'GraphQL Yoga'
 
 const Comments = dynamic(
   // @ts-ignore
-  () => import('guild-docs/giscus').then((m) => m.Giscus),
+  () => import('@theguild/components/giscus').then((m) => m.Giscus),
   { ssr: false },
 ) as Giscus
 
 const config: DocsThemeConfig = {
   titleSuffix: ` – ${SITE_NAME}`,
-  projectLink: 'https://github.com/dotansimha/graphql-yoga', // GitHub link in the navbar
+  project: {
+    link: 'https://github.com/dotansimha/graphql-yoga', // GitHub link in the navbar
+  },
   docsRepositoryBase:
     'https://github.com/dotansimha/graphql-yoga/tree/master/website/src/pages', // base URL for the docs repository
-  nextLinks: true,
-  prevLinks: true,
-  search: false,
-  floatTOC: true,
-  darkMode: true,
-  footer: false,
-  footerEditLink: 'Edit this page on GitHub',
+  search: {
+    component: null,
+  },
+  footer: {
+    component: <FooterExtended />,
+  },
+  editLink: {
+    text: 'Edit this page on GitHub',
+  },
   logo: (
     <>
       <YogaLogo className="mr-1.5 h-9 w-9" />
@@ -36,31 +47,48 @@ const config: DocsThemeConfig = {
       </div>
     </>
   ),
-  projectChatLink: 'https://discord.gg/94CDTmgmbs',
-  head: () => (
+  chat: {
+    link: 'https://discord.gg/94CDTmgmbs',
+  },
+  head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content={`${SITE_NAME}: documentation`} />
       <meta name="og:title" content={`${SITE_NAME}: documentation`} />
     </>
   ),
-  gitTimestamp: 'Last updated on',
-  defaultMenuCollapsed: true,
-  feedbackLink: 'Question? Give us feedback →',
-  feedbackLabels: 'kind/docs',
-  bodyExtraContent() {
-    const { route } = useRouter()
-    if (!route.startsWith('/docs') && !route.startsWith('/tutorial')) {
-      return null
-    }
-    return (
-      <Comments
-        repo="dotansimha/graphql-yoga"
-        repoId="MDEwOlJlcG9zaXRvcnkxMTA4MTk5Mzk="
-        category="Docs Discussion"
-        categoryId="DIC_kwDOBpr6Y84CAquY"
+  navbar: (props) => (
+    <>
+      <Header
+        accentColor="#1cc8ee"
+        themeSwitch
+        searchBarProps={{ version: 'v2' }}
       />
-    )
+      <Navbar {...props} />
+    </>
+  ),
+  sidebar: {
+    defaultMenuCollapsed: true,
+  },
+  feedback: {
+    content: 'Question? Give us feedback →',
+    labels: 'kind/docs',
+  },
+  main: {
+    extraContent() {
+      const { route } = useRouter()
+      if (route.startsWith('/docs') || route.startsWith('/tutorial')) {
+        return (
+          <Comments
+            repo="dotansimha/graphql-yoga"
+            repoId="MDEwOlJlcG9zaXRvcnkxMTA4MTk5Mzk="
+            category="Docs Discussion"
+            categoryId="DIC_kwDOBpr6Y84CAquY"
+          />
+        )
+      }
+      return null
+    },
   },
 }
 
