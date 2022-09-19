b10501346e7d96db247f98adad9d0ccdb3eccbae
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2022-09-19 12:57:13 -0400

    test: file-upload example (#1750)

---
+++ examples/file-upload/__integration-tests__/file-upload.spec.ts
@@ -0,0 +1,144 @@
+import { yoga } from '../yoga'
+import { createServer, Server } from 'http'
+import { AddressInfo } from 'net'
+import { fetch, File, FormData } from '@whatwg-node/fetch'
+import * as fs from 'fs'
+import * as crypto from 'crypto'
+import * as path from 'path'
+
+function md5File(path: string) {
+  return new Promise((resolve, reject) => {
+    const output = crypto.createHash('md5')
+    const input = fs.createReadStream(path)
+
+    input.on('error', (err) => {
+      reject(err)
+    })
+
+    output.once('readable', () => {
+      resolve(output.read().toString('hex'))
+    })
+
+    input.pipe(output)
+  })
+}
+
+describe('graphql-auth example integration', () => {
+  let server: Server
+  let port: number
+
+  beforeAll(async () => {
+    server = createServer(yoga)
+    await new Promise<void>((resolve) => server.listen(0, resolve))
+    port = (server.address() as AddressInfo).port
+  })
+
+  afterAll(async () => {
+    await new Promise((resolve) => server.close(resolve))
+  })
+
+  it('should execute query', async () => {
+    const response = await fetch(
+      `http://localhost:${port}/graphql?query=query{hello}`,
+    )
+    const body = await response.json()
+    expect(body.errors).toBeUndefined()
+    expect(body.data).toEqual({
+      hello: 'Hello World',
+    })
+  })
+
+  it('should save file', async () => {
+    const sourceFilePath = path.join(
+      __dirname,
+      '..',
+      '..',
+      '..',
+      'website',
+      'public',
+      'logo.png',
+    )
+    const sourceMd5 = await md5File(sourceFilePath)
+
+    const formData = new FormData()
+    formData.set(
+      'operations',
+      JSON.stringify({
+        query: /* GraphQL */ `
+          mutation saveFile($file: File!) {
+            saveFile(file: $file)
+          }
+        `,
+      }),
+    )
+    formData.set('map', JSON.stringify({ 0: ['variables.file'] }))
+    formData.set(
+      '0',
+      new File(
+        [await fs.promises.readFile(sourceFilePath)],
+        path.basename(sourceFilePath),
+        { type: 'image/png' },
+      ),
+    )
+
+    const response = await fetch(`http://localhost:${port}/graphql`, {
+      method: 'POST',
+      body: formData,
+    })
+
+    const body = await response.json()
+    expect(body.errors).toBeUndefined()
+    expect(body.data).toEqual({
+      saveFile: true,
+    })
+
+    const targetFilePath = path.join(__dirname, '..', 'logo.png')
+    await fs.promises.stat(targetFilePath)
+    const targetMd5 = await md5File(targetFilePath)
+    expect(targetMd5).toEqual(sourceMd5)
+    fs.promises.unlink(targetFilePath)
+    expect(targetMd5).toBe(sourceMd5)
+  })
+
+  it('should read file text', async () => {
+    const sourceFilePath = path.join(
+      __dirname,
+      '..',
+      '..',
+      '..',
+      'website',
+      'public',
+      'logo.png',
+    )
+
+    const formData = new FormData()
+    formData.set(
+      'operations',
+      JSON.stringify({
+        query: /* GraphQL */ `
+          mutation readTextFile($file: File!) {
+            readTextFile(file: $file)
+          }
+        `,
+      }),
+    )
+    formData.set('map', JSON.stringify({ 0: ['variables.file'] }))
+    formData.set(
+      '0',
+      new File(
+        [await fs.promises.readFile(sourceFilePath)],
+        path.basename(sourceFilePath),
+        { type: 'image/png' },
+      ),
+    )
+
+    const response = await fetch(`http://localhost:${port}/graphql`, {
+      method: 'POST',
+      body: formData,
+    })
+
+    const body = await response.json()
+    expect(body.errors).toBeUndefined()
+    expect(body.data).toBeDefined()
+  })
+})
--- examples/file-upload/index.ts
+++ examples/file-upload/index.ts
@@ -1,48 +1,5 @@
-import { createYoga, createSchema } from 'graphql-yoga'
 import http from 'http'
-import fs from 'fs'
-import path from 'path'
-
-const yoga = createYoga({
-  schema: createSchema({
-    typeDefs: /* GraphQL */ `
-      scalar File
-      type Query {
-        hello(name: String): String!
-      }
-      type Mutation {
-        readTextFile(file: File!): String!
-        saveFile(file: File!): Boolean!
-      }
-    `,
-    resolvers: {
-      Query: {
-        hello: (_, { name }: { name: string }) => `Hello ${name || 'World'}`,
-      },
-      Mutation: {
-        readTextFile: async (_, { file }: { file: File }) => {
-          const fileContent = await file.text()
-          return fileContent
-        },
-        saveFile: async (_, { file }: { file: File }) => {
-          try {
-            const fileStream = file.stream()
-            await fs.promises.writeFile(
-              path.join(__dirname, file.name),
-              fileStream,
-            )
-          } catch (e) {
-            return false
-          }
-          return true
-        },
-      },
-    },
-  }),
-  graphiql: {
-    title: 'Hello World',
-  },
-})
+import { yoga } from './yoga'
 
 const server = http.createServer(yoga)
 server.listen(4000)
