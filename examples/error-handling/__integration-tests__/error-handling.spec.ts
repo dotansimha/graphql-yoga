b7aac32efe22f780b0d3061fed5a36b2b9a3be55
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2022-09-19 12:17:22 -0400

    test: error handling example (#1734)
    * test: error handling example
    
    * make tests not flaky
    
    * improve
    
    * chore(dependencies): updated changesets for modified dependencies
    
    * prettier
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ examples/error-handling/__integration-tests__/error-handling.spec.ts
@@ -0,0 +1,61 @@
+import { yoga } from '../src/yoga'
+import { createServer, Server } from 'http'
+import { AddressInfo } from 'net'
+import { fetch } from '@whatwg-node/fetch'
+
+describe('error-handling example integration', () => {
+  let server: Server
+  let port: number
+
+  beforeAll(async () => {
+    server = createServer(yoga)
+    await new Promise<void>((resolve) => server.listen(0, resolve))
+    port = (server.address() as AddressInfo).port
+  })
+
+  afterAll(async () => {
+    await new Promise((resolve) => server.close(resolve))
+  })
+
+  it('should get a masked error', async () => {
+    const response = await fetch(
+      `http://localhost:${port}/graphql?query=query{greeting}`,
+    )
+    const body = await response.json()
+
+    expect(body.errors).toBeDefined()
+    expect(body.errors[0].message).toEqual('Unexpected error.')
+    expect(body.data).toBeNull()
+  })
+
+  it('should get a custom error', async () => {
+    const response = await fetch(
+      `http://localhost:${port}/graphql?query=query{user(byId: "6"){id}}`,
+    )
+    const body = await response.json()
+
+    expect(body.errors).toBeDefined()
+    expect(body.errors).toMatchInlineSnapshot(`
+        [
+          {
+            "extensions": {
+              "code": "USER_NOT_FOUND",
+              "someRandomExtensions": {
+                "aaaa": 3,
+              },
+            },
+            "locations": [
+              {
+                "column": 7,
+                "line": 1,
+              },
+            ],
+            "message": "User with id '6' not found.",
+            "path": [
+              "user",
+            ],
+          },
+        ]
+        `)
+  })
+})
