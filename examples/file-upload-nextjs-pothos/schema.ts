32faabd754b292c39fb87d98c8dcd520cb41f98b
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2022-09-19 13:52:13 -0400

    test: file-upload-nextjs-pothos example (#1752)

---
+++ examples/file-upload-nextjs-pothos/schema.ts
@@ -0,0 +1,37 @@
+import SchemaBuilder from '@pothos/core'
+
+const builder = new SchemaBuilder<{
+  Scalars: { File: { Input: File; Output: never } }
+}>({})
+
+builder.scalarType('File', {
+  serialize: () => {
+    throw new Error('Uploads can only be used as input types')
+  },
+})
+
+builder.queryType({
+  fields: (t) => ({
+    greetings: t.string({ resolve: () => 'Hello World' }),
+  }),
+})
+
+builder.mutationType({
+  fields: (t) => ({
+    readTextFile: t.string({
+      args: {
+        file: t.arg({
+          type: 'File',
+          required: true,
+        }),
+      },
+      resolve: async (_, { file }) => {
+        const textContent = await file.text()
+
+        return textContent
+      },
+    }),
+  }),
+})
+
+export const schema = builder.toSchema({})
