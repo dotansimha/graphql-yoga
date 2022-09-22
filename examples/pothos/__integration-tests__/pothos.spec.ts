817fef1530cbc58dd5520bb8e9258e804a80a9a7
Denis Badurina <badurinadenis@gmail.com>
2022-09-22 17:07:16 +0200

    pothos example w subscription (#1782)

---
+++ examples/pothos/__integration-tests__/pothos.spec.ts
@@ -0,0 +1,38 @@
+import { yoga } from '../src/yoga'
+
+describe('pothos example integration', () => {
+  it('should query', async () => {
+    const response = await yoga.fetch('/graphql?query={hello}')
+
+    expect(response.status).toBe(200)
+    expect(await response.text()).toMatchInlineSnapshot(
+      `"{"data":{"hello":"world"}}"`,
+    )
+  })
+
+  it('should subscribe', async () => {
+    const response = await yoga.fetch(
+      '/graphql?query=subscription{greetings}',
+      {
+        headers: {
+          accept: 'text/event-stream',
+        },
+      },
+    )
+
+    expect(response.status).toBe(200)
+    expect(await response.text()).toMatchInlineSnapshot(`
+      "data: {"data":{"greetings":"Hi"}}
+
+      data: {"data":{"greetings":"Bonjour"}}
+
+      data: {"data":{"greetings":"Hola"}}
+
+      data: {"data":{"greetings":"Ciao"}}
+
+      data: {"data":{"greetings":"Zdravo"}}
+
+      "
+    `)
+  })
+})
---
+++ examples/pothos/src/yoga.ts
@@ -0,0 +1,36 @@
+import { createYoga } from 'graphql-yoga'
+import SchemaBuilder from '@pothos/core'
+
+const builder = new SchemaBuilder({})
+
+builder.queryType({
+  fields: (t) => ({
+    hello: t.string({
+      args: {
+        name: t.arg.string({ required: true, defaultValue: 'world' }),
+      },
+      resolve: (_parent, { name }) => name,
+    }),
+  }),
+})
+
+builder.subscriptionType({
+  fields: (t) => ({
+    greetings: t.string({
+      async *subscribe() {
+        yield 'Hi'
+        yield 'Bonjour'
+        yield 'Hola'
+        yield 'Ciao'
+        yield 'Zdravo'
+      },
+      resolve(parent) {
+        return parent
+      },
+    }),
+  }),
+})
+
+export const yoga = createYoga({
+  schema: builder.toSchema(),
+})
