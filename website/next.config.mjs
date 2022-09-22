92b48d5ba7a7cca9829fee034e38d07bca6b4524
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-09-22 04:29:27 -0400

    Update next.config.mjs

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,6 +1,9 @@
 import { withGuildDocs } from '@theguild/components/next.config'
 
 export default withGuildDocs({
+  images: {
+    unoptimized: true, // doesn't work with `next export`
+  },
   redirects: () =>
     Object.entries({
       '/docs/quick-start': '/docs',
