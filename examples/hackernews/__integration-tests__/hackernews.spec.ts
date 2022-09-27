55523340d3c67c8ea9e3404c6c1d2cb73bc97746
enisdenjo <badurinadenis@gmail.com>
2022-09-27 18:34:05 +0200

    don't mock prisma

--- examples/hackernews/__integration-tests__/hackernews.spec.ts
+++ examples/hackernews/__integration-tests__/hackernews.spec.ts
@@ -1,66 +1,68 @@
-import { createServer, Server } from 'http'
-import { AddressInfo } from 'net'
-import { fetch } from '@whatwg-node/fetch'
+import { DbDrop, MigrateDev } from '@prisma/migrate'
+import { PrismaClient } from '@prisma/client'
 import { createYoga } from 'graphql-yoga'
 import { schema } from '../src/schema'
-import { GraphQLContext } from '../src/context'
-import { mockDeep } from 'jest-mock-extended'
-import { PrismaClient } from '@prisma/client'
-
-const createMockContext = (): GraphQLContext => {
-  return {
-    prisma: mockDeep<PrismaClient>(),
-  }
-}
+import { createContext } from '../src/context'
 
 describe('hackernews example integration', () => {
-  let server: Server
-  let port: number
-  let mockCtx: GraphQLContext
-
   beforeAll(async () => {
-    mockCtx = createMockContext()
-    const yoga = createYoga({
-      schema,
-      context: createMockContext,
-      maskedErrors: false,
+    // migrate
+    await MigrateDev.new().parse([])
+
+    // seed
+    const client = new PrismaClient()
+    await client.link.create({
+      data: {
+        url: 'https://www.prisma.io',
+        description: 'Prisma replaces traditional ORMs',
+      },
     })
-    server = createServer(yoga)
-    await new Promise<void>((resolve) => server.listen(0, resolve))
-    port = (server.address() as AddressInfo).port
+    await client.$disconnect()
   })
 
   afterAll(async () => {
-    await new Promise((resolve) => server.close(resolve))
+    // drop
+    await DbDrop.new().parse([
+      '--preview-feature', // experimental feature
+      '--force',
+    ])
   })
 
-  it('should execute query', async () => {
-    const response = await fetch(
-      `http://localhost:${port}/graphql?query=query{hello}`,
+  const yoga = createYoga({ schema, context: createContext })
+
+  it('should get posts from feed', async () => {
+    const response = await yoga.fetch(
+      'http://yoga/graphql?query={feed{url,description}}',
     )
+
     const body = await response.json()
-    expect(body.errors).toBeUndefined()
-    expect(body.data).toEqual({
-      hello: 'Hello World!',
-    })
+    expect(body).toMatchInlineSnapshot(`
+      {
+        "data": {
+          "feed": [
+            {
+              "description": "Prisma replaces traditional ORMs",
+              "url": "https://www.prisma.io",
+            },
+          ],
+        },
+      }
+    `)
   })
 
-  it('should create a post', async () => {
-    mockCtx.prisma.link.create.mockResolvedValue({
-      url: 'https://www.prisma.io',
-      description: 'Prisma replaces traditional ORMs',
-    })
-    const response = await fetch(`http://localhost:${port}/graphql`, {
+  it('should create a new post', async () => {
+    const response = await yoga.fetch('http://yoga/graphql', {
       method: 'POST',
       headers: { 'content-type': 'application/json' },
       body: JSON.stringify({
         query: /* GraphQL */ `
           mutation createPost {
             postLink(
-              url: "https://www.prisma.io"
-              description: "Prisma replaces traditional ORMs"
+              url: "https://www.the-guild.dev/graphql/yoga-server"
+              description: "Time to Relax with GraphQL Yoga"
             ) {
               url
+              description
             }
           }
         `,
@@ -68,9 +70,15 @@ describe('hackernews example integration', () => {
     })
 
     const body = await response.json()
-    expect(body.errors).toBeUndefined()
-    expect(body.data).toEqual({
-      url: 'https://www.prisma.io',
-    })
+    expect(body).toMatchInlineSnapshot(`
+      {
+        "data": {
+          "postLink": {
+            "description": "Time to Relax with GraphQL Yoga",
+            "url": "https://www.the-guild.dev/graphql/yoga-server",
+          },
+        },
+      }
+    `)
   })
 })
