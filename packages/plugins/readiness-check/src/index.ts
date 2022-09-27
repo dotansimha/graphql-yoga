98d42ea52b4ee791c0dc230c6ffc7432c9a612a9
enisdenjo <badurinadenis@gmail.com>
2022-09-27 22:21:45 +0200

    use readiness check plugin

---
+++ packages/plugins/readiness-check/src/index.ts
@@ -0,0 +1,62 @@
+import { Plugin } from 'graphql-yoga'
+
+export interface ReadinessCheckPluginOptions {
+  /**
+   * Under which endpoint do you want the readiness check to be?
+   *
+   * @default /ready
+   */
+  endpoint?: string
+  /**
+   * The check for whether the service is ready to perform.
+   *
+   * You should check here whether the services Yoga depends on
+   * are ready and working, for example: is the database up and running?
+   *
+   * - Returning `true` or nothing will respond with a 200 OK.
+   * - Returning `false` or throwing an error will respond with a 503 Service Unavailable.
+   * - Returning a `Response` will have the readiness check respond with it.
+   *
+   * Beware that if an instance of `Error` is thrown, its message will be present in the
+   * response body. Be careful which information you expose.
+   */
+  check: (
+    request: Request,
+  ) => void | boolean | Response | Promise<void | boolean | Response>
+}
+
+/**
+ * Adds a readiness check for Yoga by simply implementing the `check` option.
+ */
+export function useReadinessCheck({
+  endpoint = '/ready',
+  check,
+}: ReadinessCheckPluginOptions): Plugin {
+  return {
+    async onRequest({ request, endResponse, fetchAPI, url }) {
+      const { pathname: requestPath } = url
+      if (requestPath === endpoint) {
+        let response: Response
+        try {
+          const readyOrResponse = await check(request)
+          if (typeof readyOrResponse === 'object') {
+            response = readyOrResponse
+          } else {
+            response = new fetchAPI.Response(null, {
+              status: readyOrResponse === false ? 503 : 200,
+            })
+          }
+        } catch (err) {
+          const isError = err instanceof Error
+          response = new fetchAPI.Response(isError ? err.message : null, {
+            status: 503,
+            headers: isError
+              ? { 'content-type': 'text/plain; charset=utf-8' }
+              : {},
+          })
+        }
+        endResponse(response)
+      }
+    },
+  }
+}
