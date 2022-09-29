6210e1f22051f70cfc5ba3b3320cf603e3694bf2
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2022-09-29 06:11:28 -0400

    Update examples/netlify-edge/__integration-tests__/netlify-edge.spec.ts
    Co-authored-by: Denis Badurina <badurinadenis@gmail.com>

--- examples/netlify-edge/__integration-tests__/netlify-edge.spec.ts
+++ examples/netlify-edge/__integration-tests__/netlify-edge.spec.ts
@@ -4,22 +4,9 @@ import { AddressInfo } from 'net'
 import { fetch } from '@whatwg-node/fetch'
 
 describe('netlify-edge example integration', () => {
-  let server: Server
-  let port: number
-
-  beforeAll(async () => {
-    server = createServer(yoga)
-    await new Promise<void>((resolve) => server.listen(0, resolve))
-    port = (server.address() as AddressInfo).port
-  })
-
-  afterAll(async () => {
-    await new Promise((resolve) => server.close(resolve))
-  })
-
   it('should execute query', async () => {
-    const response = await fetch(
-      `http://localhost:${port}/graphql?query=query{greetings}`,
+    const response = await yoga.fetch(
+      'http://yoga/graphql?query=query{greetings}',
     )
     const body = await response.json()
     expect(body.errors).toBeUndefined()
