ac8a4993fcd7267bc72f3923f6ee4c3d41331f1c
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2022-09-30 15:46:36 -0400

    test: graphql armor

--- examples/graphql-armor/src/main.ts
+++ examples/graphql-armor/src/main.ts
@@ -1,40 +1,5 @@
-import { createSchema, createYoga } from 'graphql-yoga'
-import { EnvelopArmor } from '@escape.tech/graphql-armor'
 import { createServer } from 'http'
-
-const armor = new EnvelopArmor()
-const enhancements = armor.protect()
-
-const booksStore = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-]
-
-const yoga = createYoga({
-  plugins: [...enhancements.plugins],
-  schema: createSchema({
-    typeDefs: /* GraphQL */ `
-      type Book {
-        title: String
-        author: String
-      }
-      type Query {
-        books: [Book]
-      }
-    `,
-    resolvers: {
-      Query: {
-        books: () => booksStore,
-      },
-    },
-  }),
-})
+import { yoga } from './yoga'
 
 const server = createServer(yoga)
 server.listen(4000, () => {
