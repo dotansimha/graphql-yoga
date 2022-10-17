1a1273c19e25b6c3603b501285840c68d8d82edd
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2022-10-17 14:44:54 -0400

    not sure what to do with types cause it should work

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@theguild/components/style.css'
-import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function App({ Component, pageProps }: any) {
   return <Component {...pageProps} />
 }
 
