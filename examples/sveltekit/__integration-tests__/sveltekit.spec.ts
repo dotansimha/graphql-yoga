6364751fb60d3c5ed328eefaf066e70da0516f88
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-10-18 17:30:45 +0300

    No yarn just 50 cent

--- examples/sveltekit/__integration-tests__/sveltekit.spec.ts
+++ examples/sveltekit/__integration-tests__/sveltekit.spec.ts
@@ -33,7 +33,7 @@ describe('SvelteKit integration', () => {
 			execSync('pnpm workspace example-sveltekit build');
 
 			// Start sveltekit
-			sveltekitProcess = spawn('yarn', ['workspace', 'example-sveltekit', 'preview']);
+			sveltekitProcess = spawn('pnpm', ['workspace', 'example-sveltekit', 'preview']);
 
 			// Wait for sveltekit to start
 			await new Promise((resolve) => setTimeout(resolve, timings.setup.waitAfterPreview));
