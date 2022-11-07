41be4578ff232fb7fb53eadc4733693a1a27f903
YassinEldeeb <yassineldeeb94@gmail.com>
2022-11-07 13:10:11 +0200

    test: make a failing test case for using variables

--- packages/graphql-yoga/__tests__/400.spec.ts
+++ packages/graphql-yoga/__tests__/400.spec.ts
@@ -1,16 +1,22 @@
 import { createSchema } from '../src/schema'
 import { createYoga } from '../src/server'
+import { DateTimeResolver, DateTimeTypeDefinition } from 'graphql-scalars'
+import { expect } from '@jest/globals'
 
 describe('400 status code errors', () => {
   it('Handle invalid data type usage errors as 400', async () => {
     const yoga = createYoga({
       schema: createSchema({
         typeDefs: /* GraphQL */ `
+          ${DateTimeTypeDefinition}
           type Query {
             ok: Boolean!
           }
           type Mutation {
-            sendMessage(userId: Int!): String!
+            sendMessage(data: MessageInput!): String!
+          }
+          input MessageInput {
+            sendingDate: DateTime
           }
         `,
         resolvers: {
@@ -22,6 +28,7 @@ describe('400 status code errors', () => {
           Query: {
             ok: () => true,
           },
+          DateTime: DateTimeResolver,
         },
       }),
       maskedErrors: false,
@@ -31,15 +38,17 @@ describe('400 status code errors', () => {
       method: 'POST',
       headers: { 'content-type': 'application/json' },
       body: JSON.stringify({
-        query: 'mutation { sendMessage(userId: "Simply not a user id") }',
+        query: 'mutation($data: MessageInput!) { sendMessage(data: $data) }',
+        variables: { data: { sendingDate: 'SOME TEXT' } },
       }),
     })
 
     expect(await response.json()).toMatchObject({
       errors: [
         {
-          message:
-            'Int cannot represent non-integer value: "Simply not a user id"',
+          message: expect.stringContaining(
+            'DateTime cannot represent an invalid date-time-string SOME TEXT.',
+          ),
         },
       ],
     })
