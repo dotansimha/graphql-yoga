d619dee7edece1c15f85549706a104bc1c0ea613
Denis Badurina <denis@domonda.com>
2022-11-08 11:36:21 +0100

    dedent graphiql placeholder text

--- packages/graphiql/src/YogaGraphiQL.tsx
+++ packages/graphiql/src/YogaGraphiQL.tsx
@@ -62,39 +62,39 @@ export type YogaGraphiQLProps = Omit<
 
 export function YogaGraphiQL(props: YogaGraphiQLProps): React.ReactElement {
   const initialQuery = /* GraphQL */ `#
-  # Welcome to ${props.title || 'Yoga GraphiQL'}
-  #
-  # ${
+# Welcome to ${props.title || 'Yoga GraphiQL'}
+#
+# ${
     props.title || 'Yoga GraphiQL'
   } is an in-browser tool for writing, validating, and
-  # testing GraphQL queries.
-  #
-  # Type queries into this side of the screen, and you will see intelligent
-  # typeaheads aware of the current GraphQL type schema and live syntax and
-  # validation errors highlighted within the text.
-  #
-  # GraphQL queries typically start with a "{" character. Lines that start
-  # with a # are ignored.
-  #
-  # An example GraphQL query might look like:
-  #
-  #     {
-  #       field(arg: "value") {
-  #         subField
-  #       }
-  #     }
-  #
-  # Keyboard shortcuts:
-  #
-  #  Prettify Query:  Shift-Ctrl-P (or press the prettify button above)
-  #
-  #     Merge Query:  Shift-Ctrl-M (or press the merge button above)
-  #
-  #       Run Query:  Ctrl-Enter (or press the play button above)
-  #
-  #   Auto Complete:  Ctrl-Space (or just start typing)
-  #
-  `
+# testing GraphQL queries.
+#
+# Type queries into this side of the screen, and you will see intelligent
+# typeaheads aware of the current GraphQL type schema and live syntax and
+# validation errors highlighted within the text.
+#
+# GraphQL queries typically start with a "{" character. Lines that start
+# with a # are ignored.
+#
+# An example GraphQL query might look like:
+#
+#     {
+#       field(arg: "value") {
+#         subField
+#       }
+#     }
+#
+# Keyboard shortcuts:
+#
+#  Prettify Query:  Shift-Ctrl-P (or press the prettify button above)
+#
+#     Merge Query:  Shift-Ctrl-M (or press the merge button above)
+#
+#       Run Query:  Ctrl-Enter (or press the play button above)
+#
+#   Auto Complete:  Ctrl-Space (or just start typing)
+#
+`
 
   const endpoint = new URL(
     props.endpoint ?? location.pathname,
