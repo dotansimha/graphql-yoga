6f0016f8017f1c16083ab67d2ef587ee55a71c96
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-11-08 14:37:53 +0300

    Drop unused `execa` dependency (#2052)

--- jest.config.js
+++ jest.config.js
@@ -42,11 +42,9 @@ module.exports = {
   restoreMocks: true,
   reporters: ['default'],
   modulePathIgnorePatterns: ['dist'],
-  moduleNameMapper: {
-    ...pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
-      prefix: `${ROOT_DIR}/`,
-    }),
-  },
+  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
+    prefix: `${ROOT_DIR}/`,
+  }),
   collectCoverage: false,
   cacheDirectory: resolve(ROOT_DIR, `${CI ? '' : 'node_modules/'}.cache/jest`),
   testMatch,
