17c0b2a7dd1200dde1f71d53a613501ba75bc1e0
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-11-08 13:46:10 +0300

    Drop chalk dependency

--- examples/sofa/src/index.ts
+++ examples/sofa/src/index.ts
@@ -1,6 +1,6 @@
 import { yoga } from './yoga'
 import { createServer } from 'http'
-import * as chalk from 'chalk'
+import { titleBold, infoColor } from 'graphql-yoga'
 
 const server = createServer(yoga)
 
@@ -8,24 +8,22 @@ server.listen(4000, async () => {
   const url = `http://localhost:4000`
 
   function printUrl(path: string) {
-    return chalk.gray(url + path)
+    return infoColor(url + path)
   }
 
   console.log(`
-    ${chalk.bold('Swagger UI: ')}    ${printUrl('/swagger')}
+    ${titleBold('Swagger UI: ')}    ${printUrl('/swagger')}
 
-    ${chalk.bold('GraphQL:')}        ${printUrl('/graphql')}
+    ${titleBold('GraphQL:')}        ${printUrl('/graphql')}
 
-    ${chalk.bold('Queries:')}
+    ${titleBold('Queries:')}
       me:           ${printUrl('/rest/me')}
       users:        ${printUrl('/rest/users')}
       user:         ${printUrl('/rest/user/1')}
       books:        ${printUrl('/rest/books')}
       book:         ${printUrl('/rest/book/1')}
 
-    ${chalk.bold('Mutations:')}
-      addBook:      ${printUrl('/rest/add-book')} ${chalk.italic.gray(
-    'POST: {title}',
-  )}
+    ${titleBold('Mutations:')}
+      addBook:      ${printUrl('/rest/add-book')} ${infoColor('POST: {title}')}
   `)
 })
