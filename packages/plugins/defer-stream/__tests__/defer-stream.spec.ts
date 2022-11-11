5c0afe29c663aeaa7703054fa7613cde473566e2
Laurin Quast <laurinquast@googlemail.com>
2022-11-11 09:07:39 +0100

    split tests

--- packages/plugins/defer-stream/__tests__/defer-stream.spec.ts
+++ packages/plugins/defer-stream/__tests__/defer-stream.spec.ts
@@ -6,8 +6,7 @@ import {
   GraphQLSchema,
   GraphQLString,
 } from 'graphql'
-import { createServer } from 'node:http'
-import { fetch } from '@whatwg-node/fetch'
+import { createPushPullAsyncIterable } from './push-pull-async-iterable'
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
@@ -26,9 +25,9 @@ const schema = new GraphQLSchema({
         type: new GraphQLList(GraphQLString),
         async *resolve() {
           yield 'A'
-          await new Promise((resolve) => setTimeout(resolve, 1000))
+          await new Promise((resolve) => setTimeout(resolve, 5))
           yield 'B'
-          await new Promise((resolve) => setTimeout(resolve, 1000))
+          await new Promise((resolve) => setTimeout(resolve, 5))
           yield 'C'
         },
       },
@@ -230,94 +229,6 @@ describe('Defer/Stream', () => {
     }
   })
 
-  it('correctly deals with the source upon aborted requests (real world)', async () => {
-    const { source, push, terminate } = createPushPullAsyncIterable<string>()
-    push('A')
-
-    const yoga = createYoga({
-      schema: createSchema({
-        typeDefs: /* GraphQL */ `
-          type Query {
-            hi: [String]
-          }
-        `,
-        resolvers: {
-          Query: {
-            hi: () => source,
-          },
-        },
-      }),
-      plugins: [useDeferStream()],
-    })
-
-    const server = createServer(yoga)
-
-    try {
-      await new Promise<void>((resolve) => {
-        server.listen(() => {
-          resolve()
-        })
-      })
-
-      const port = (server.address() as any)?.port ?? null
-      if (port === null) {
-        throw new Error('Missing port...')
-      }
-
-      const response = await fetch(`http://localhost:${port}/graphql`, {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json',
-          accept: 'multipart/mixed',
-        },
-        body: JSON.stringify({
-          query: '{ hi @stream }',
-        }),
-      })
-      let counter = 0
-      const toStr = (arr: Uint8Array) => Buffer.from(arr).toString('utf-8')
-      for await (const chunk of response.body!) {
-        const parts = toStr(chunk)
-          .split('\r\n')
-          .filter((p) => p.startsWith('{'))
-        for (const part of parts) {
-          if (counter === 0) {
-            expect(part).toBe(`{"data":{"hi":[]},"hasNext":true}`)
-          } else if (counter === 1) {
-            expect(part).toBe(
-              `{"incremental":[{"items":["A"],"path":["hi",0]}],"hasNext":true}`,
-            )
-            push('B')
-          } else if (counter === 2) {
-            expect(part).toBe(
-              `{"incremental":[{"items":["B"],"path":["hi",1]}],"hasNext":true}`,
-            )
-            push('C')
-          } else if (counter === 3) {
-            expect(part).toBe(
-              `{"incremental":[{"items":["C"],"path":["hi",2]}],"hasNext":true}`,
-            )
-            // when the source is returned this stream/loop should be exited.
-            terminate()
-            push('D')
-          } else if (counter === 4) {
-            expect(part).toBe(`{"hasNext":false}`)
-          } else {
-            throw new Error("LOL, this shouldn't happen.")
-          }
-
-          counter++
-        }
-      }
-    } finally {
-      await new Promise<void>((res) => {
-        server.close(() => {
-          res()
-        })
-      })
-    }
-  })
-
   describe('Content-Types', () => {
     it('accept: <void>', async () => {
       const yoga = createYoga({
@@ -383,7 +294,8 @@ describe('Defer/Stream', () => {
         body: JSON.stringify({ query: '{ hi @stream }' }),
       })
       expect(response.status).toEqual(406)
-      // TODO: what else?
+      expect(response.headers.get('content-type')).toEqual(null)
+      expect(await response.text()).toEqual('')
     })
     it('accept: multipart/mixed', async () => {
       const yoga = createYoga({
@@ -456,56 +368,3 @@ describe('Defer/Stream', () => {
     })
   })
 })
-
-type Deferred<T = void> = {
-  resolve: (value: T) => void
-  reject: (value: unknown) => void
-  promise: Promise<T>
-}
-
-function createDeferred<T = void>(): Deferred<T> {
-  const d = {} as Deferred<T>
-  d.promise = new Promise<T>((resolve, reject) => {
-    d.resolve = resolve
-    d.reject = reject
-  })
-  return d
-}
-
-const createPushPullAsyncIterable = <T>(): {
-  source: AsyncGenerator<T>
-  push: (item: T) => void
-  terminate: () => void
-} => {
-  const queue: Array<T> = []
-  let d = createDeferred()
-  let terminated = false
-
-  const source = new Repeater<T>(async (push, stop) => {
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-      if (terminated) {
-        stop()
-        return
-      }
-      let item: T | undefined
-      while ((item = queue.shift())) {
-        push(item)
-      }
-      await d.promise
-    }
-  })
-
-  return {
-    source,
-    push: (item) => {
-      queue.push(item)
-      d.resolve()
-      d = createDeferred()
-    },
-    terminate: () => {
-      terminated = true
-      d.resolve()
-    },
-  }
-}
