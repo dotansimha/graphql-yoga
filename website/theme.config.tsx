a7ae238b8be8453408f1226cce15c41437ae6a4b
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-12-01 15:53:25 +0100

    update to next 13

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -2,35 +2,12 @@
 import { useTheme, Giscus, defineConfig, YogaLogo } from '@theguild/components'
 import { useRouter } from 'next/router'
 
-const siteName = 'GraphQL Yoga'
-
 export default defineConfig({
   chat: {
     link: 'https://discord.gg/94CDTmgmbs',
   },
   docsRepositoryBase:
     'https://github.com/dotansimha/graphql-yoga/tree/main/website',
-  getNextSeoProps() {
-    return {
-      openGraph: {
-        images: [
-          { url: 'https://the-guild.dev/graphql/yoga-server/cover.png' },
-        ],
-      },
-    }
-  },
-  logo: (
-    <>
-      <YogaLogo className="mr-1.5 h-9 w-9" />
-      <div>
-        <h1 className="md:text-md text-sm font-medium">{siteName}</h1>
-        <h2 className="hidden text-xs sm:block">
-          Fully-featured, simple to set up, performant and extendable GraphQL
-          JavaScript server
-        </h2>
-      </div>
-    </>
-  ),
   main({ children }) {
     const { resolvedTheme } = useTheme()
     const { route } = useRouter()
@@ -55,5 +32,5 @@ export default defineConfig({
       </>
     )
   },
-  siteName,
+  siteName: 'YOGA',
 })
