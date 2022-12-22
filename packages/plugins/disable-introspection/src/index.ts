c5b1cc46f67c4516fcaeb6247f56da4ca7dd7511
Laurin Quast <laurinquast@googlemail.com>
2022-12-22 12:34:41 +0100

    feat: disable introspection plugin (#2231)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ packages/plugins/disable-introspection/src/index.ts
@@ -0,0 +1,27 @@
+import type { Plugin, PromiseOrValue } from 'graphql-yoga'
+import { NoSchemaIntrospectionCustomRule } from 'graphql'
+
+type UseDisableIntrospectionArgs = {
+  isDisabled?: (request: Request) => PromiseOrValue<boolean>
+}
+
+const store = new WeakMap<Request, boolean>()
+
+export const useDisableIntrospection = (
+  props?: UseDisableIntrospectionArgs,
+): Plugin => {
+  return {
+    async onRequest({ request }) {
+      const isDisabled = props?.isDisabled
+        ? await props.isDisabled(request)
+        : true
+      store.set(request, isDisabled)
+    },
+    onValidate({ addValidationRule, context }) {
+      const isDisabled = store.get(context.request) ?? true
+      if (isDisabled) {
+        addValidationRule(NoSchemaIntrospectionCustomRule)
+      }
+    },
+  }
+}
