b230a9377713eb6e4f353bc8399a2a96afda4a8e
Denis Badurina <badurinadenis@gmail.com>
2023-01-05 15:37:01 +0100

    CSRF prevention plugin (#2292)

---
+++ packages/plugins/csrf-prevention/__tests__/csrf-prevention.spec.ts
@@ -0,0 +1,60 @@
+import { createSchema, createYoga } from 'graphql-yoga'
+import { useCSRFPrevention } from '../src/index.js'
+
+describe('csrf-prevention', () => {
+  const schema = createSchema({
+    typeDefs: /* GraphQL */ `
+      type Query {
+        hello: String!
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello() {
+          return 'world'
+        },
+      },
+    },
+  })
+
+  const yoga = createYoga({
+    schema,
+    plugins: [
+      useCSRFPrevention({
+        requestHeaders: ['x-graphql-yoga-csrf', 'x-some-other-required'],
+      }),
+    ],
+    maskedErrors: false,
+  })
+
+  it('should not allow requests without the necessary header', async () => {
+    const res = await yoga.fetch('http://yoga/graphql?query={hello}', {
+      'x-not-the-required': 'header',
+    })
+
+    expect(res.status).toBe(403)
+    expect(res.statusText).toMatchInlineSnapshot(`"Forbidden"`)
+  })
+
+  it('should allow requests with the necessary header', async () => {
+    let res = await yoga.fetch('http://yoga/graphql?query={hello}', {
+      headers: {
+        'x-graphql-yoga-csrf': 'whatevs',
+      },
+    })
+    expect(res.status).toBe(200)
+    await expect(res.text()).resolves.toMatchInlineSnapshot(
+      `"{"data":{"hello":"world"}}"`,
+    )
+
+    res = await yoga.fetch('http://yoga/graphql?query={hello}', {
+      headers: {
+        'x-some-other-required': 'whatevs',
+      },
+    })
+    expect(res.status).toBe(200)
+    await expect(res.text()).resolves.toMatchInlineSnapshot(
+      `"{"data":{"hello":"world"}}"`,
+    )
+  })
+})
