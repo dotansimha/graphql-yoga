6a1481d2a8288c6595124261bd4ea6e93822c1af
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-01-31 17:53:53 +0300

    Go

--- benchmark/hello-world/start-server.js
+++ benchmark/hello-world/start-server.js
@@ -11,39 +11,74 @@ globalThis.URL = class URL {
     if (!this._parsed) {
       this._parsed = fastUri.parse(this.urlStr)
     }
+    return this._parsed
   }
   get hash() {
-    return this.parsed.fragment
+    return this.parsed.fragment || ''
+  }
+  set hash(value) {
+    this.parsed.fragment = value
   }
   get host() {
     return this.parsed.host
   }
+  set host(value) {
+    this.parsed.host = value
+  }
   get hostname() {
     return this.parsed.host
   }
+  set hostname(value) {
+    this.parsed.host = value
+  }
   get href() {
     return fastUri.serialize(this.parsed)
+  }
+  set href(value) {
+    this.urlStr = value
+    this._parsed = null
   }
   toString() {
     return this.href
   }
   get origin() {
-    return this.parsed.protocol + '//' + this.parsed.host
+    return fastUri.serialize({
+      scheme: this.parsed.scheme,
+      host: this.parsed.host,
+    })
+  }
+  set origin(value) {
+    // no-op
   }
   get password() {
-    throw new Error('Not implemented')
+    return undefined
+  }
+  set password(value) {
+    // no-op
   }
   get pathname() {
     return this.parsed.path
   }
+  set pathname(value) {
+    this.parsed.path = value
+  }
   get port() {
     return this.parsed.port
+  }
+  set port(value) {
+    this.parsed.port = value
   }
   get protocol() {
     return this.parsed.scheme
   }
+  set protocol(value) {
+    this.parsed.scheme = value
+  }
   get search() {
-    return this.parsed.query
+    return this.parsed.query || ''
+  }
+  set search(value) {
+    this.parsed.query = value
   }
   get searchParams() {
     if (!this._searchParams) {
@@ -52,7 +87,10 @@ globalThis.URL = class URL {
     return this._searchParams
   }
   get username() {
-    throw new Error('Not implemented')
+    return undefined
+  }
+  set username(value) {
+    // no-op
   } 
   toJSON() {
     return this.href
@@ -75,8 +113,9 @@ const yoga = createYoga({
       },
     },
   }),
-  logging: false,
+  logging: true,
   multipart: false,
+  maskedErrors: true,
 })
 
 const server = createServer(yoga)
