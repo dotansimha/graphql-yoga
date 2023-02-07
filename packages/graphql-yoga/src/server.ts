b342f259fba0dac78e052c9589d4cc40567bcbe2
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-02-07 16:16:55 +0300

    TS COME ON

--- packages/graphql-yoga/src/server.ts
+++ packages/graphql-yoga/src/server.ts
@@ -149,7 +149,7 @@ export type YogaServerOptions<TServerContext, TUserContext> = {
 
   parserCache?: boolean | ParserAndValidationCacheOptions
   validationCache?: boolean | ValidationCache
-  fetchAPI?: Record<keyof FetchAPI, any>
+  fetchAPI?: Partial<Record<keyof FetchAPI, any>>
   /**
    * GraphQL Multipart Request spec support
    *
