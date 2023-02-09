e4352c5535fe9f63717e2ecdf0974155f63248c7
davy-dev <68077611+davy-dev@users.noreply.github.com>
2023-02-09 18:12:20 +0100

    Update graphql.ts with createYoga...
    I replace getSession by  getServerSession

--- examples/nextjs-auth/pages/api/graphql.ts
+++ examples/nextjs-auth/pages/api/graphql.ts
@@ -1,28 +1,63 @@
-import { createServer } from '@graphql-yoga/node'
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { createYoga, createSchema } from 'graphql-yoga'
+
 import type { NextApiRequest, NextApiResponse } from 'next'
 import type { Session } from 'next-auth'
 import  { getServerSession } from "next-auth/next"
 import  { authOptions } from './api/auth/[...nextauth]'
- 
+
 export const config = {
   api: {
     // Disable body parsing (required for file uploads)
-    bodyParser: false
-  }
+    bodyParser: false,
+  },
 }
- 
-export default createServer<
+
+export default createYoga<
   {
     req: NextApiRequest
     res: NextApiResponse
   },
   {
-    session: Session | null
+    session: Session
   }
 >({
-  context: async ({ req,res }) => {
-    
-    return { session: await getServerSession(req,res,authOptions)}
-    
-  }
+  context: async ({ req, res }) => {
+    return {
+      session: await getServerSession(req, res , authOptions),
+    }
+  },
+  schema: createSchema({
+    typeDefs: /* GraphQL */ `
+      type User {
+        id: String!
+        name: String!
+        email: String!
+        image: String!
+      }
+      type Session {
+        user: User!
+        expires: String!
+      }
+      type Query {
+        session: Session
+      }
+    `,
+    resolvers: {
+      Query: {
+        session(_source, _args, context) {
+          return context.session ?? null
+        },
+      },
+      User: {
+        id(source) {
+          return source['email']
+        },
+      },
+    },
+  }),
+  graphiql: {
+    defaultQuery: `query Session { session { expires user { id email image } } }`,
+  },
+  graphqlEndpoint: '/api/graphql',
 })
