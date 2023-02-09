38777cb6c8bca57b822ae266792d6413d5714c28
davy-dev <68077611+davy-dev@users.noreply.github.com>
2023-02-09 17:20:24 +0100

    Update graphql.ts
    replace getSession which is available only on client side  to getServerssion.

--- examples/nextjs-auth/pages/api/graphql.ts
+++ examples/nextjs-auth/pages/api/graphql.ts
@@ -1,64 +1,28 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { createYoga, createSchema } from 'graphql-yoga'
-
+import { createServer } from '@graphql-yoga/node'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import type { Session } from 'next-auth'
-import { getSession } from 'next-auth/react'
-
+import  { getServerSession } from "next-auth/next"
+import  { authOptions } from './api/auth/[...nextauth]'
+ 
 export const config = {
   api: {
     // Disable body parsing (required for file uploads)
-    bodyParser: false,
-  },
+    bodyParser: false
+  }
 }
-
-export default createYoga<
+ 
+export default createServer<
   {
     req: NextApiRequest
     res: NextApiResponse
   },
   {
-    session: Session
+    session: Session | null
   }
 >({
-  context: async ({ req }) => {
-    return {
-      session: await getSession({ req }),
-    }
-  },
-  schema: createSchema({
-    typeDefs: /* GraphQL */ `
-      type User {
-        id: String!
-        name: String!
-        email: String!
-        image: String!
-      }
-
-      type Session {
-        user: User!
-        expires: String!
-      }
-
-      type Query {
-        session: Session
-      }
-    `,
-    resolvers: {
-      Query: {
-        session(_source, _args, context) {
-          return context.session ?? null
-        },
-      },
-      User: {
-        id(source) {
-          return source['email']
-        },
-      },
-    },
-  }),
-  graphiql: {
-    defaultQuery: `query Session { session { expires user { id email image } } }`,
-  },
-  graphqlEndpoint: '/api/graphql',
+  context: async ({ req,res }) => {
+    
+    return { session: await getServerSession(req,res,authOptions)}
+    
+  }
 })
