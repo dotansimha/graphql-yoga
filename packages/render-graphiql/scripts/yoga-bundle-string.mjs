ff029432372a595b6a370ec865f175998e135cb7
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-02-17 12:51:30 +0300

    enhance(render-graphiql): offline favicon

--- packages/render-graphiql/scripts/yoga-bundle-string.mjs
+++ packages/render-graphiql/scripts/yoga-bundle-string.mjs
@@ -6,16 +6,22 @@ const directoryName = path.dirname(fileURLToPath(import.meta.url))
 const inputPath = path.resolve(directoryName, '..', '..', 'graphiql', 'dist')
 const jsFile = path.resolve(inputPath, 'yoga-graphiql.umd.js')
 const cssFile = path.resolve(inputPath, 'style.css')
+const faviconFile = path.resolve(directoryName, '../../../website/public/favicon.ico')
 
 const outFile = path.resolve(directoryName, '..', 'src', 'graphiql.ts')
 
 const jsContents = fs.readFileSync(jsFile, 'utf-8')
 const cssContents = fs.readFileSync(cssFile, 'utf-8')
+const faviconContents = `data:image/x-icon;base64,${fs.readFileSync(
+  faviconFile,
+  'base64',
+)}`
 
 fs.writeFileSync(
   outFile,
   [
     `export const js: string = ${JSON.stringify(jsContents)}`,
     `export const css: string = ${JSON.stringify(cssContents)}`,
+    `export const favicon: string = ${JSON.stringify(faviconContents)}`,
   ].join('\n'),
 )
