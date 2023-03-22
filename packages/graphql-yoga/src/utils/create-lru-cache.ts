edf56073e182ac6855c595235796747b6f32b32b
Laurin Quast <laurinquast@googlemail.com>
2023-03-22 08:42:21 +0100

    Revert "fix(deps): update dependency lru-cache to v8 (#2559)"
    This reverts commit 46e75917f784abcb4ea9968bf3d73660df157b21.

--- packages/graphql-yoga/src/utils/create-lru-cache.ts
+++ packages/graphql-yoga/src/utils/create-lru-cache.ts
@@ -3,7 +3,6 @@ import LRU from 'lru-cache'
 const DEFAULT_MAX = 1024
 const DEFAULT_TTL = 3_600_000
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-export function createLRUCache<T extends {}>() {
+export function createLRUCache<T>() {
   return new LRU<string, T>({ max: DEFAULT_MAX, ttl: DEFAULT_TTL })
 }
