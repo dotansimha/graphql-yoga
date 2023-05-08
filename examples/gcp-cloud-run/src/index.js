2b8e0644dc0e98350cda0f617856d8c9af0abc72
Valentin Cocaud <v.cocaud@gmail.com>
2023-05-08 14:49:42 +0200

    Add an example for GCP Cloud Run (#2737)
    * add example for GCP Cloud Run
    
    * fix example package.json

---
+++ examples/gcp-cloud-run/src/index.js
@@ -0,0 +1,27 @@
+import { createSchema, createYoga } from 'graphql-yoga'
+import { createServer } from 'node:http'
+
+const yoga = createYoga({
+  schema: createSchema({
+    typeDefs: /* GraphQL */ `
+      type Query {
+        greetings: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        greetings: () =>
+          'This is the `greetings` field of the root `Query` type',
+      },
+    },
+  }),
+})
+
+const server = createServer(yoga)
+const port = parseInt(process.env.PORT) || 4000
+
+server.listen(port, () => {
+  console.info(
+    `Server is running on http://localhost:${port}${yoga.graphqlEndpoint}`,
+  )
+})
