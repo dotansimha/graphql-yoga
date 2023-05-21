79212e1385cd4b379ff0505f9ea27b6bdb86ed93
Valentin Cocaud <v.cocaud@gmail.com>
2023-05-21 17:27:39 +0200

    poc with integration test instead of e2e

---
+++ examples/nextjs-app/__integration-tests__/nestjs-app.spec.ts
@@ -0,0 +1,112 @@
+import { rejects } from 'node:assert'
+import { exec } from 'node:child_process'
+import { join, resolve } from 'node:path'
+import { Readable, finished } from 'node:stream'
+
+describe('nextjs 13 App Router', () => {
+  it('should show GraphiQL', async () => {
+    const response = await fetch('http://localhost:3000/api/graphql', {
+      headers: {
+        accept: 'text/html',
+      },
+    })
+
+    expect(response.ok).toBe(true)
+    expect(await response.text()).toContain('<title>Yoga GraphiQL</title>')
+  })
+
+  it('should run basic query', async () => {
+    const response = await fetch('http://localhost:3000/api/graphql', {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: 'query { greetings }',
+      }),
+    })
+
+    expect(response.ok).toBe(true)
+
+    const json = await response.json()
+
+    expect(json.errors).toBeFalsy()
+    expect(json.data?.greetings).toBe(
+      'This is the `greetings` field of the root `Query` type',
+    )
+  })
+
+  jest.setTimeout(1000 * 60 * 5)
+  let serverProcess: ReturnType<typeof cmd>
+  let buildProcess: ReturnType<typeof cmd>
+
+  beforeAll(async () => {
+    buildProcess = cmd('pnpm build')
+    await buildProcess.exited
+    serverProcess = cmd('pnpm start')
+    await waitForEndpoint('http://localhost:3000', 5, 500)
+  })
+
+  afterAll(async () => {
+    await buildProcess?.stop()
+    await serverProcess?.stop().catch(() => {})
+  })
+})
+
+function cmd(cmd: string) {
+  const cp = exec(cmd, {
+    cwd: join(module.path, '..'),
+    timeout: 1000 * 60 * 1,
+  })
+  cp.stdout?.on('data', (data: any) => console.log(data))
+  cp.stderr?.on('data', (data: any) => console.error(data))
+
+  const exited = new Promise<void>((resolve, reject) => {
+    cp.on('close', async (code: number) => (code == 0 ? resolve() : reject()))
+    cp.on('error', (error) => {
+      console.error(error)
+      reject()
+    })
+  })
+
+  return {
+    exited,
+    stop: () => {
+      cp.kill()
+      return exited
+    },
+  }
+}
+
+export async function waitForEndpoint(
+  endpoint: string,
+  retries: number,
+  timeout = 10000,
+): Promise<boolean> {
+  for (let attempt = 1; attempt <= retries; attempt++) {
+    console.info(
+      `Trying to connect to ${endpoint} (attempt ${attempt}/${retries})...`,
+    )
+    try {
+      const r = await fetch(endpoint)
+
+      if (!r.ok) {
+        throw new Error(`Endpoint not ready yet, status code is ${r.status}`)
+      }
+
+      return true
+    } catch (e) {
+      console.warn(
+        `Failed to connect to endpoint: ${endpoint}, waiting ${timeout}ms...`,
+        (e as any).message,
+      )
+
+      await new Promise((resolve) => setTimeout(resolve, timeout))
+    }
+  }
+
+  throw new Error(
+    `Failed to connect to endpoint: ${endpoint} (attempts: ${retries})`,
+  )
+}
