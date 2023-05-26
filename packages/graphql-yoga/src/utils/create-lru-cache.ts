6f83c0c520fda0a2c01745e0589570ca1abbc16c
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-05-26 17:41:42 +0300

    Update LRU Cache package (#2801)
    * Update LRU Cache
    
    * chore(dependencies): updated changesets for modified dependencies
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- packages/graphql-yoga/src/utils/create-lru-cache.ts
+++ packages/graphql-yoga/src/utils/create-lru-cache.ts
@@ -1,8 +1,18 @@
-import LRU from 'lru-cache'
+/* eslint-disable @typescript-eslint/ban-types */
+import { LRUCache as LRU } from 'lru-cache'
 
 const DEFAULT_MAX = 1024
 const DEFAULT_TTL = 3_600_000
 
-export function createLRUCache<T>() {
-  return new LRU<string, T>({ max: DEFAULT_MAX, ttl: DEFAULT_TTL })
+export type LRUCache<T extends {}> = LRU<string, T>
+export interface LRUCacheOptions {
+  max?: number
+  ttl?: number
+}
+
+export function createLRUCache<T extends {}>({
+  max = DEFAULT_MAX,
+  ttl = DEFAULT_TTL,
+}: LRUCacheOptions = {}) {
+  return new LRU<string, T>({ max, ttl })
 }
