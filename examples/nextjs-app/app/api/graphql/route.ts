465c50b52526ec906da9b91ccc84fbb767cf5f04
Valentin Cocaud <v.cocaud@gmail.com>
2023-05-26 16:42:30 +0200

    Update NextJS examples and docuamentation for new App Directory (#2760)
    * move legacy next example
    
    * add example for NextJS 13 new app directory
    
    * update next integration documentation
    
    * update Next specific wording to match Next's doc
    
    * Update examples/nextjs-legacy-pages/package.json
    
    Co-authored-by: Laurin Quast <laurinquast@googlemail.com>
    
    * fix documentation to pass production build
    
    * fix versel e2e tests
    
    * Update
    
    * fix documentation for nextjs
    
    * fix nextjs example
    
    * remove unecessary Request param
    
    ---------
    
    Co-authored-by: Laurin Quast <laurinquast@googlemail.com>
    Co-authored-by: Arda TANRIKULU <ardatanrikulu@gmail.com>

---
+++ examples/nextjs-app/app/api/graphql/route.ts
@@ -0,0 +1,26 @@
+// Next.js Custom Route Handler: https://nextjs.org/docs/app/building-your-application/routing/router-handlers
+import { createYoga, createSchema } from 'graphql-yoga'
+
+const { handleRequest } = createYoga({
+  schema: createSchema({
+    typeDefs: /* GraphQL */ `
+      type Query {
+        greetings: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        greetings: () =>
+          'This is the `greetings` field of the root `Query` type',
+      },
+    },
+  }),
+
+  // While using Next.js file convention for routing, we need to configure Yoga to use the correct endpoint
+  graphqlEndpoint: '/api/graphql',
+
+  // Yoga needs to know how to create a valid Next response
+  fetchAPI: { Response },
+})
+
+export { handleRequest as GET, handleRequest as POST }
