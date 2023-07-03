5542ac33c64ce5896983782251907767553a42ab
Valentin Cocaud <v.cocaud@gmail.com>
2023-07-03 18:13:30 +0200

    remove unnecessary shcema part

--- examples/express-helmet/src/app.ts
+++ examples/express-helmet/src/app.ts
@@ -6,33 +6,13 @@ export function buildApp(app: ReturnType<typeof express>) {
   const graphQLServer = createYoga({
     schema: createSchema({
       typeDefs: /* GraphQL */ `
-        scalar File
         type Query {
           hello: String
-        }
-        type Mutation {
-          getFileName(file: File!): String
-        }
-        type Subscription {
-          countdown(from: Int!): Int!
         }
       `,
       resolvers: {
         Query: {
           hello: () => 'world',
-        },
-        Mutation: {
-          getFileName: (root, { file }: { file: File }) => file.name,
-        },
-        Subscription: {
-          countdown: {
-            async *subscribe(_, { from }) {
-              for (let i = from; i >= 0; i--) {
-                await new Promise((resolve) => setTimeout(resolve, 1000))
-                yield { countdown: i }
-              }
-            },
-          },
         },
       },
     }),
