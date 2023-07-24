c5d4dc9873f01d94af94b4ad2d499298cb33c3a2
Valentin Cocaud <v.cocaud@gmail.com>
2023-07-24 14:22:23 +0200

    use RS256 as default algorithm

--- packages/plugins/jwt/src/__tests__/jwt.spec.ts
+++ packages/plugins/jwt/src/__tests__/jwt.spec.ts
@@ -1,102 +1,91 @@
-import { createSchema, createYoga } from 'graphql-yoga'
-import jwt from 'jsonwebtoken'
-import crypto from 'node:crypto'
-
-import { JwtPluginOptions, useJWT } from '@graphql-yoga/plugin-jwt'
+import crypto from 'node:crypto';
+import { createSchema, createYoga } from 'graphql-yoga';
+import jwt from 'jsonwebtoken';
+import { JwtPluginOptions, useJWT } from '@graphql-yoga/plugin-jwt';
 
 describe('jwt', () => {
   it('should throw if no signing key or jwksUri is provided', () => {
     // @ts-expect-error testing invalid options fo JS users
     expect(() => useJWT({ issuer: 'yoga' })).toThrow(
       'You need to provide either a signingKey or a jwksUri',
-    )
-  })
+    );
+  });
 
   it('should throw if both signing key and jwksUri are provided', () => {
     expect(() =>
       // @ts-expect-error testing invalid options fo JS users
       useJWT({ signingKey: 'test', jwksUri: 'test', issuer: 'yoga' }),
-    ).toThrow('You need to provide either a signingKey or a jwksUri, not both')
-  })
+    ).toThrow('You need to provide either a signingKey or a jwksUri, not both');
+  });
 
   it('should throw on unsupported header type', async () => {
-    const server = createTestServer()
+    const server = createTestServer();
 
     await expect(server.queryWithAuth('Basic 123')).rejects.toMatchObject({
       message: 'Unsupported token type provided: "Basic"',
       extensions: { http: { status: 401 } },
-    })
-  })
+    });
+  });
 
   it('should throw on invalid token', async () => {
-    const server = createTestServer()
+    const server = createTestServer();
 
     await expect(server.queryWithAuth('Bearer abcd')).rejects.toMatchObject({
       message: 'Failed to decode authentication token',
       extensions: { http: { status: 401 } },
-    })
-  })
+    });
+  });
 
   it('should not accept token without algorithm', async () => {
-    const server = createTestServer()
+    const server = createTestServer();
 
-    await expect(
-      server.queryWithAuth(buildJWTWithoutAlg()),
-    ).rejects.toMatchObject({
+    await expect(server.queryWithAuth(buildJWTWithoutAlg())).rejects.toMatchObject({
       message: 'Failed to decode authentication token',
       extensions: { http: { status: 401 } },
-    })
-  })
+    });
+  });
 
   it('should not allow non matching issuer', async () => {
-    const server = createTestServer()
+    const server = createTestServer();
 
-    await expect(
-      server.queryWithAuth(buildJWT({ iss: 'test' })),
-    ).rejects.toMatchObject({
+    await expect(server.queryWithAuth(buildJWT({ iss: 'test' }))).rejects.toMatchObject({
       message: 'Failed to decode authentication token',
       extensions: { http: { status: 401 } },
-    })
-  })
+    });
+  });
 
   it('should not allow non matching audience', async () => {
-    const server = createTestServer({ audience: 'test' })
+    const server = createTestServer({ audience: 'test' });
 
-    await expect(
-      server.queryWithAuth(buildJWT({ aud: 'wrong' })),
-    ).rejects.toMatchObject({
+    await expect(server.queryWithAuth(buildJWT({ aud: 'wrong' }))).rejects.toMatchObject({
       message: 'Failed to decode authentication token',
       extensions: { http: { status: 401 } },
-    })
-  })
+    });
+  });
 
   it('should verify token against provided jwsk', async () => {
-    const server = createTestServer({ signingKey: undefined, jwksUri: 'test' })
+    const server = createTestServer({ signingKey: undefined, jwksUri: 'test' });
 
-    const response = await server.queryWithAuth(buildJWT({}, { keyid: 'yoga' }))
+    const response = await server.queryWithAuth(buildJWT({}, { keyid: 'yoga' }));
 
-    expect(response.status).toBe(200)
-  })
+    expect(response.status).toBe(200);
+  });
 
   it('should not allow unknown key id', async () => {
-    const server = createTestServer({ signingKey: undefined, jwksUri: 'test' })
+    const server = createTestServer({ signingKey: undefined, jwksUri: 'test' });
 
-    await expect(
-      server.queryWithAuth(buildJWT({}, { keyid: 'unknown' })),
-    ).rejects.toMatchObject({
+    await expect(server.queryWithAuth(buildJWT({}, { keyid: 'unknown' }))).rejects.toMatchObject({
       message: 'Failed to decode authentication token. Unknown key id.',
       extensions: { http: { status: 401 } },
-    })
-  })
+    });
+  });
 
   it('should give access to the jwt payload in the context', async () => {
-    const server = createTestServer()
+    const server = createTestServer();
 
-    const response = await server.queryWithAuth(
-      buildJWT({ claims: { test: 'test' } }),
-    )
+    const response = await server.queryWithAuth(buildJWT({ claims: { test: 'test' } }));
 
-    expect(response.status).toBe(200)
+    expect(response.status).toBe(200);
     expect(await response.json()).toMatchObject({
       data: {
         ctx: {
@@ -108,17 +97,15 @@ describe('jwt', () => {
           },
         },
       },
-    })
-  })
+    });
+  });
 
   it('should allow to customize the constructor', async () => {
-    const server = createTestServer({ extendContextField: 'custom' })
+    const server = createTestServer({ extendContextField: 'custom' });
 
-    const response = await server.queryWithAuth(
-      buildJWT({ claims: { test: 'test' } }),
-    )
+    const response = await server.queryWithAuth(buildJWT({ claims: { test: 'test' } }));
 
-    expect(response.status).toBe(200)
+    expect(response.status).toBe(200);
     expect(await response.json()).toMatchObject({
       data: {
         ctx: {
@@ -127,18 +114,18 @@ describe('jwt', () => {
           },
         },
       },
-    })
-  })
+    });
+  });
 
   it('should allow to get token from something else than headers', async () => {
     const server = createTestServer({
       getToken: () => buildJWT({}).split(' ')[1],
-    })
+    });
 
-    const response = await server.queryWithAuth('')
-    expect(response.status).toBe(200)
-  })
-})
+    const response = await server.queryWithAuth('');
+    expect(response.status).toBe(200);
+  });
+});
 
 const createTestServer = (options?: Partial<JwtPluginOptions>) => {
   const yoga = createYoga({
@@ -148,10 +135,11 @@ const createTestServer = (options?: Partial<JwtPluginOptions>) => {
       useJWT({
         issuer: 'http://yoga',
         signingKey: 'very secret key',
+        algorithms: ['HS256'],
         ...options,
       }),
     ],
-  })
+  });
 
   return {
     yoga,
@@ -165,8 +153,8 @@ const createTestServer = (options?: Partial<JwtPluginOptions>) => {
           authorization,
         },
       }),
-  }
-}
+  };
+};
 
 const schema = createSchema({
   typeDefs: /* GraphQL */ `
@@ -180,7 +168,7 @@ const schema = createSchema({
       ctx: (_, __, ctx) => ctx,
     },
   },
-})
+});
 
 const body = JSON.stringify({
   query: /* GraphQL */ `
@@ -188,39 +176,36 @@ const body = JSON.stringify({
       ctx
     }
   `,
-})
+});
 
 const buildJWT = (
   payload: object,
   options: Parameters<typeof jwt.sign>[2] & { key?: string } = {},
 ) => {
-  const { key = 'very secret key', ...signOptions } = options
+  const { key = 'very secret key', ...signOptions } = options;
 
-  const token = jwt.sign({ iss: 'http://yoga', ...payload }, key, signOptions)
+  const token = jwt.sign({ iss: 'http://yoga', ...payload }, key, signOptions);
 
-  return `Bearer ${token}`
-}
+  return `Bearer ${token}`;
+};
 
 function buildJWTWithoutAlg(payload: object = {}, key = 'very secret key') {
-  const header = Buffer.from(JSON.stringify({ typ: 'JWT' })).toString('base64')
-  const encodedPayload = Buffer.from(
-    JSON.stringify({ iss: 'http://yoga', ...payload }),
-  ).toString('base64')
-  const encodedJWT = `${header}.${encodedPayload}`
-  const signature = crypto
-    .createHmac('sha256', key)
-    .update(encodedJWT)
-    .digest('base64')
-  return `Bearer ${encodedJWT}.${signature}`
+  const header = Buffer.from(JSON.stringify({ typ: 'JWT' })).toString('base64');
+  const encodedPayload = Buffer.from(JSON.stringify({ iss: 'http://yoga', ...payload })).toString(
+    'base64',
+  );
+  const encodedJWT = `${header}.${encodedPayload}`;
+  const signature = crypto.createHmac('sha256', key).update(encodedJWT).digest('base64');
+  return `Bearer ${encodedJWT}.${signature}`;
 }
 
 jest.mock('jwks-rsa', () => ({
   JwksClient: jest.fn(() => ({
     getSigningKey: jest.fn((kid: string) => {
       if (kid !== 'yoga') {
-        return null
+        return null;
       }
-      return { getPublicKey: () => 'very secret key' }
+      return { getPublicKey: () => 'very secret key' };
     }),
   })),
-}))
+}));
