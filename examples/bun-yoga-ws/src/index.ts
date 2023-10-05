6f471bbc28514f127e1f3e76c77b41a0fc64afab
ashbuilds <ashish.mishra41191@gmail.com>
2023-10-05 21:33:30 +0200

    chore: add example - use yoga with bun and websockets

---
+++ examples/bun-yoga-ws/src/index.ts
@@ -0,0 +1,90 @@
+import Bun from 'bun';
+import { makeHandler } from 'graphql-ws/lib/use/bun';
+import { createSchema, createYoga } from 'graphql-yoga';
+
+const schema = createSchema({
+  typeDefs: /* GraphQL */ `
+    type Query {
+      greetings: String
+    }
+    type Subscription {
+      countdown(from: Int!): Int!
+      dynamicLoading(loadTime: Int!): String!
+    }
+  `,
+  resolvers: {
+    Query: {
+      greetings: () => 'Hello Bun!',
+    },
+    Subscription: {
+      dynamicLoading: {
+        async *subscribe(_, { loadTime }) {
+          let counter = 0;
+          const spinnerFrames = ['\u25D4', '\u25D1', '\u25D5', '\u25D0'];
+          while (counter < loadTime) {
+            await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for a second
+            yield { dynamicLoading: `Loading ${spinnerFrames[counter % spinnerFrames.length]}` };
+            counter++;
+          }
+          yield { dynamicLoading: 'Loaded \u2713' };
+        },
+      },
+    },
+  },
+});
+
+const yoga = createYoga({
+  schema,
+  graphiql: {
+    subscriptionsProtocol: 'WS', // use WebSockets instead of SSE
+  },
+});
+
+export const websocketHandler = makeHandler({
+  schema,
+  execute: args => args.rootValue.execute(args),
+  subscribe: args => args.rootValue.subscribe(args),
+  onSubscribe: async (ctx, msg) => {
+    const { schema, execute, subscribe, contextFactory, parse, validate } = yoga.getEnveloped({
+      ...ctx,
+      req: ctx.extra.request,
+      socket: ctx.extra.socket,
+      params: msg.payload,
+    });
+
+    const args = {
+      schema,
+      operationName: msg.payload.operationName,
+      document: parse(msg.payload.query),
+      variableValues: msg.payload.variables,
+      contextValue: await contextFactory(),
+      rootValue: {
+        execute,
+        subscribe,
+      },
+    };
+
+    const errors = validate(args.schema, args.document);
+    if (errors.length) return errors;
+    return args;
+  },
+});
+
+const server: Bun.Server = Bun.serve({
+  fetch: (request: Request, server: Bun.Server): Promise<Response> | Response => {
+    // Upgrade the request to a WebSocket
+    if (server.upgrade(request)) {
+      return new Response();
+    }
+    return yoga.fetch(request, server);
+  },
+  port: 4000,
+  websocket: websocketHandler,
+});
+
+console.info(
+  `🚀 Server is running on ${new URL(
+    yoga.graphqlEndpoint,
+    `http://${server.hostname}:${server.port}`,
+  )}`,
+);
