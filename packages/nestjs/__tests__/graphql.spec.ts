d66cf9326fc6b8be1987ab3f6fbe02e43eab0051
Jérémy Magrin <jeremy.magrin@gmail.com>
2023-10-05 11:51:33 +0200

    feat: add test

--- packages/nestjs/__tests__/graphql.spec.ts
+++ packages/nestjs/__tests__/graphql.spec.ts
@@ -1,3 +1,11 @@
+import {
+  GraphQLID,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from 'graphql';
 import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { fetch } from '@whatwg-node/fetch';
@@ -7,7 +15,44 @@ let app: INestApplication, url: string;
 
 beforeAll(async () => {
   const module = await Test.createTestingModule({
-    imports: [AppModule.forRoot()],
+    imports: [
+      AppModule.forRoot({
+        conditionalSchema: async () => {
+          const DogObjectType = new GraphQLObjectType({
+            name: 'Dog',
+            fields: {
+              id: {
+                type: new GraphQLNonNull(GraphQLID),
+              },
+              name: {
+                type: new GraphQLNonNull(GraphQLString),
+              },
+              breed: {
+                type: new GraphQLNonNull(GraphQLString),
+              },
+            },
+          });
+
+          return new GraphQLSchema({
+            query: new GraphQLObjectType({
+              name: 'Query',
+              fields: {
+                getDogs: {
+                  type: new GraphQLList(new GraphQLNonNull(DogObjectType)),
+                  resolve: () => [
+                    {
+                      id: '1',
+                      name: 'Bingo',
+                      breed: 'Dalmatian',
+                    },
+                  ],
+                },
+              },
+            }),
+          });
+        },
+      }),
+    ],
   }).compile();
   app = module.createNestApplication();
   await app.listen(0);
@@ -42,6 +87,39 @@ it('should return query result', async () => {
             "color": "black",
             "id": 1,
             "weight": 5,
+          },
+        ],
+      },
+    }
+  `);
+});
+
+it('should return query result for conditional schema', async () => {
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: /* GraphQL */ `
+        {
+          getDogs {
+            id
+            name
+            breed
+          }
+        }
+      `,
+    }),
+  });
+  await expect(res.json()).resolves.toMatchInlineSnapshot(`
+    {
+      "data": {
+        "getDogs": [
+          {
+            "breed": "Dalmatian",
+            "id": "1",
+            "name": "Bingo",
           },
         ],
       },
