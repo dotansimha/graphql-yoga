9dd1a94b45ecfe2cc36f441ce7447857f1d6f64f
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-10-09 00:47:20 +0200

    prettier

--- website/src/lib/v3-get-static-props.ts
+++ website/src/lib/v3-get-static-props.ts
@@ -1,34 +1,34 @@
-import { GetStaticPaths, GetStaticProps } from 'next'
-import { buildDynamicMDX, buildDynamicMeta } from 'nextra/remote'
-import { defaultRemarkPlugins } from '@theguild/components/next.config'
-import json from '../../remote-files/v3.json' assert { type: 'json' }
-import { remarkLinkRewrite } from 'nextra/mdx-plugins'
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { remarkLinkRewrite } from 'nextra/mdx-plugins';
+import { buildDynamicMDX, buildDynamicMeta } from 'nextra/remote';
+import { defaultRemarkPlugins } from '@theguild/components/next.config';
+import json from '../../remote-files/v3.json';
 
-const { user, repo, branch, docsPath, filePaths } = json
+const { user, repo, branch, docsPath, filePaths } = json;
 
 export const getStaticPaths: GetStaticPaths = async () => ({
   fallback: 'blocking',
   paths: filePaths
     // this fix `TOO MANY REDIRECTS` error in CF
-    .map((filePath) => filePath.replace(/\/?index\.mdx?$/, ''))
-    .map((filePath) => ({
+    .map(filePath => filePath.replace(/\/?index\.mdx?$/, ''))
+    .map(filePath => ({
       params: { slug: filePath.replace(/\.mdx?$/, '').split('/') },
     })),
-})
+});
 
 export const getStaticProps: GetStaticProps<
   {
-    __nextra_dynamic_mdx: string
-    __nextra_dynamic_opts: string
+    __nextra_dynamic_mdx: string;
+    __nextra_dynamic_opts: string;
   },
   { slug?: string[] }
 > = async ({ params }) => {
-  const path = params?.slug?.join('/') ?? 'index'
-  const foundPath = filePaths.find((filePath) => filePath.startsWith(path))
+  const path = params?.slug?.join('/') ?? 'index';
+  const foundPath = filePaths.find(filePath => filePath.startsWith(path));
 
-  const baseURL = `https://raw.githubusercontent.com/${user}/${repo}/${branch}/${docsPath}${foundPath}`
-  const response = await fetch(baseURL)
-  const data = await response.text()
+  const baseURL = `https://raw.githubusercontent.com/${user}/${repo}/${branch}/${docsPath}${foundPath}`;
+  const response = await fetch(baseURL);
+  const data = await response.text();
 
   const mdx = await buildDynamicMDX(data, {
     defaultShowCopyCode: true,
@@ -38,12 +38,12 @@ export const getStaticProps: GetStaticProps<
         [remarkLinkRewrite, { pattern: /^\/docs(\/.*)?$/, replace: '/v2$1' }],
       ],
     },
-  })
+  });
 
   return {
     props: {
       ...mdx,
       ...(await buildDynamicMeta()),
     },
-  }
-}
+  };
+};
