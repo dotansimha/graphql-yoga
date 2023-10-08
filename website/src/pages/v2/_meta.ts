f92f38be8315239ad09ce61219de28ed0a483816
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-10-09 00:46:00 +0200

    nextra v3

--- website/src/pages/v2/_meta.ts
+++ website/src/pages/v2/_meta.ts
@@ -1,6 +1,6 @@
-import { createCatchAllMeta } from 'nextra/catch-all'
-import json from '../../../remote-files/v2.json' assert { type: 'json' }
+import { createCatchAllMeta } from 'nextra/catch-all';
+import json from '../../../remote-files/v2.json';
 
 export default async () => {
-  return createCatchAllMeta(json.filePaths, json.nestedMeta)
-}
+  return createCatchAllMeta(json.filePaths, json.nestedMeta);
+};
