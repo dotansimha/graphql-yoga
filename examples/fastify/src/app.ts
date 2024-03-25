201bf9bff8c3833b51e0d167287c6ec74db38b91
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-03-25 15:02:32 +0300

    Try the new alpha fastify

--- examples/fastify/src/app.ts
+++ examples/fastify/src/app.ts
@@ -102,7 +102,7 @@ export function buildApp(logging = true) {
     url: graphQLServer.graphqlEndpoint,
     method: ['GET', 'POST', 'OPTIONS'],
     handler: async (req, reply) => {
-      const response = await graphQLServer.handleNodeRequestFromResponse(reply, {
+      const response = await graphQLServer.handleNodeRequestAndResponse(req, reply, {
         req,
         reply,
       });
