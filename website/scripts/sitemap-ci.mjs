d21b7a361727e1e70b8e427b71794753530c6b5f
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-04-07 13:36:27 +0200

    fix sitemap-ci.mjs

--- website/scripts/sitemap-ci.mjs
+++ website/scripts/sitemap-ci.mjs
@@ -1,22 +1,21 @@
 import * as fs from 'node:fs';
-import { dirname } from 'node:path';
 import * as path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { XMLParser } from 'fast-xml-parser';
 import config from '../next.config.js';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const sitemapPath = path.join(__dirname, '..', 'public', 'sitemap.xml');
+const sitemapPath = path.join(__dirname, '..', 'out', 'sitemap.xml');
 const lockfilePath = path.join(__dirname, '..', 'route-lockfile.txt');
 
 async function main() {
   const parser = new XMLParser();
 
-  const d = parser.parse(fs.readFileSync(sitemapPath, 'utf-8'));
+  const d = parser.parse(fs.readFileSync(sitemapPath, 'utf8'));
 
   const routes = d.urlset.url.map(url =>
-    url.loc.replace(process.env.SITE_URL || `https://graphql-yoga.com`, ``),
+    url.loc.replace(process.env.SITE_URL || 'https://graphql-yoga.com', ''),
   );
 
   const redirectsPointingToNonExistingStuff = [];
