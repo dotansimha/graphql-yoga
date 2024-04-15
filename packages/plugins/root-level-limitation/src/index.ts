e83084c766622f1a3f4b6cc174c5d9f00d94d36a
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-04-15 10:56:40 +0300

    Root Level Limit: different take with onValidate

--- packages/plugins/root-level-limitation/src/index.ts
+++ packages/plugins/root-level-limitation/src/index.ts
@@ -1,79 +1,47 @@
-import { createGraphQLError } from '@graphql-tools/utils';
+import { createGraphQLError, getRootTypes } from '@graphql-tools/utils';
+import { Plugin } from '@envelop/core';
+import type { ValidationRule } from 'graphql/validation/ValidationContext';
+import { isObjectType } from 'graphql';
 
-interface GraphQLParams {
-  operationName?: string;
-  query?: string;
+export interface RootLevelQueryLimitOptions {
+  maxRootLevelFields: number;
 }
 
-export function rootLevelQueryLimit({ maxRootLevelFields }: { maxRootLevelFields: number }) {
-  return {
-    onParams({ params }: unknown) {
-      const { query, operationName } = params as GraphQLParams;
+export function createRootLevelQueryLimitRule(opts: RootLevelQueryLimitOptions): ValidationRule {
+  const { maxRootLevelFields } = opts;
 
-      if (operationName?.includes('IntrospectionQuery')) return true;
-
-      const newQuery = formatQuery(query || '');
-      const linesArray = newQuery.split('\n');
-
-      let countLeadingSpacesTwo = 0;
-
-      for (const line of linesArray) {
-        const leadingSpaces = line?.match(/^\s*/)?.[0]?.length || 0;
-
-        if (leadingSpaces === 4 && line[leadingSpaces] !== ')') {
-          countLeadingSpacesTwo++;
-
-          if (countLeadingSpacesTwo > maxRootLevelFields * 2) {
+  return function rootLevelQueryLimitRule (context) {
+    const rootTypes = getRootTypes(context.getSchema());
+    let rootFieldCount = 0;
+    return {
+      Field() {
+        const parentType = context.getParentType();
+        if (isObjectType(parentType) && rootTypes.has(parentType)) {
+          rootFieldCount++;
+          if (rootFieldCount > maxRootLevelFields) {
             throw createGraphQLError('Query is too complex.', {
               extensions: {
                 http: {
                   spec: false,
                   status: 400,
-                  headers: {
-                    Allow: 'POST',
-                  },
                 },
               },
             });
           }
         }
-      }
-
-      return true;
-    },
+      },
+    };
   };
+
 }
 
-function formatQuery(queryString: string) {
-  queryString = queryString.replace(/^\s+/gm, '');
-
-  let indentLevel = 0;
-  let formattedString = '';
-
-  for (let i = 0; i < queryString.length; i++) {
-    const char = queryString[i];
-
-    if (char === '{' || char === '(') {
-      formattedString += char;
-      indentLevel++;
-      // formattedString += ' '.repeat(indentLevel * 4);
-    } else if (char === '}' || char === ')') {
-      indentLevel--;
-
-      if (formattedString[formattedString.length - 1] !== '\n')
-        formattedString = formattedString.trim().replace(/\n$/, '');
-
-      if (char === ')') formattedString += char;
-
-      if (char === '}') formattedString += '\n' + ' '.repeat(indentLevel * 4) + char;
-    } else if (char === '\n') {
-      if (queryString[i + 1] !== '\n' && queryString[i + 1] !== undefined) {
-        formattedString += char + ' '.repeat(indentLevel * 4);
-      }
-    } else {
-      formattedString += char;
+export function rootLevelQueryLimit(opts: RootLevelQueryLimitOptions): Plugin {
+  const rootLevelQueryLimitRule = createRootLevelQueryLimitRule(opts);
+  return {
+    onValidate({ addValidationRule }) {
+      addValidationRule(
+        rootLevelQueryLimitRule
+      )
     }
   }
-
-  return formattedString;
 }
