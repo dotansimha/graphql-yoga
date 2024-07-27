c46fa37adbab9bcd33db0e6a43bbd854b582536b
Valentin Cocaud <v.cocaud@gmail.com>
2024-07-27 20:27:02 +0200

    [prometheus] rename metric options names

--- packages/plugins/prometheus/src/index.ts
+++ packages/plugins/prometheus/src/index.ts
@@ -8,6 +8,7 @@ import {
   createSummary,
   PrometheusTracingPluginConfig as EnvelopPrometheusTracingPluginConfig,
   FillLabelsFnParams,
+  getHistogramFromConfig,
   HistogramAndLabels,
   SummaryAndLabels,
   usePrometheus as useEnvelopPrometheus,
@@ -24,7 +25,9 @@ export {
 };
 
 export interface PrometheusTracingPluginConfig extends EnvelopPrometheusTracingPluginConfig {
-  http?: boolean | string | ReturnType<typeof createHistogram>;
+  metrics: EnvelopPrometheusTracingPluginConfig['metrics'] & {
+    graphql_yoga_http_duration?: boolean | string | ReturnType<typeof createHistogram>;
+  };
 
   /**
    * The endpoint to serve metrics exposed by this plugin.
@@ -36,48 +39,9 @@ export interface PrometheusTracingPluginConfig extends EnvelopPrometheusTracingP
 export function usePrometheus(options: PrometheusTracingPluginConfig): Plugin {
   const endpoint = options.endpoint || '/metrics';
   const registry = options.registry || defaultRegistry;
-  let httpHistogram: ReturnType<typeof createHistogram> | undefined;
-
-  function labelExists(label: string) {
-    if ((options.labels as Record<string, boolean>)?.[label] == null) {
-      return true;
-    }
-    return (options.labels as Record<string, boolean>)[label];
-  }
-
-  if (options.http) {
-    const labelNames = ['method', 'statusCode'];
-    if (labelExists('operationName')) {
-      labelNames.push('operationName');
-    }
-    if (labelExists('operationType')) {
-      labelNames.push('operationType');
-    }
-    httpHistogram =
-      typeof options.http === 'object'
-        ? options.http
-        : createHistogram({
-            registry,
-            histogram: {
-              name: typeof options.http === 'string' ? options.http : 'graphql_yoga_http_duration',
-              help: 'Time spent on HTTP connection',
-              labelNames,
-            },
-            fillLabelsFn(params, { request, response }) {
-              const labels: Record<string, string> = {
-                method: request.method,
-                statusCode: response.status,
-              };
-              if (labelExists('operationType') && params.operationType) {
-                labels.operationType = params.operationType;
-              }
-              if (labelExists('operationName')) {
-                labels.operationName = params.operationName || 'Anonymous';
-              }
-              return labels;
-            },
-          });
-  }
+  const httpHistogram = getHistogramFromConfig(options, 'graphql_yoga_http_duration', {
+    help: 'Time spent on HTTP connection',
+  });
 
   const startByRequest = new WeakMap<Request, number>();
   const paramsByRequest = new WeakMap<Request, FillLabelsFnParams>();
