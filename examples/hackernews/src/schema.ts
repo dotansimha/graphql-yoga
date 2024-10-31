63ea9e129c280df5ffb226ddb0ce07e334e89fc2
gilgardosh <gilgardosh@gmail.com>
2024-10-31 13:32:28 +0200

    Initial update

--- examples/hackernews/src/schema.ts
+++ examples/hackernews/src/schema.ts
@@ -1,6 +1,7 @@
-import { createGraphQLError, createSchema } from 'graphql-yoga';
+import { createSchema } from 'graphql-yoga';
 import { Prisma, type Link } from '@prisma/client';
 import type { GraphQLContext } from './context';
+import { GraphQLError } from 'graphql';
 
 const typeDefinitions = /* GraphQL */ `
   type Link {
@@ -13,12 +14,14 @@ const typeDefinitions = /* GraphQL */ `
   type Comment {
     id: ID!
     body: String!
+    link: Link
   }
 
   type Query {
-    hello: String!
+    info: String!
     feed(filterNeedle: String, skip: Int, take: Int): [Link!]!
     comment(id: ID!): Comment
+    link(id: ID!): Link
   }
 
   type Mutation {
@@ -34,9 +37,16 @@ const parseIntSafe = (value: string): number | null => {
   return null;
 };
 
+const applySkipConstraints = (value: number) => {
+  if (value < 0) {
+    throw new GraphQLError(`'skip' argument value '${value}' is invalid, value must be positive.`);
+  }
+  return value;
+};
+
 const applyTakeConstraints = (params: { min: number; max: number; value: number }) => {
   if (params.value < params.min || params.value > params.max) {
-    throw createGraphQLError(
+    throw new GraphQLError(
       `'take' argument value '${params.value}' is outside the valid range of '${params.min}' to '${params.max}'.`,
     );
   }
@@ -45,7 +55,7 @@ const applyTakeConstraints = (params: { min: number; max: number; value: number 
 
 const resolvers = {
   Query: {
-    hello: () => `Hello World!`,
+    info: () => `This is the API of a Hackernews Clone`,
     feed: async (
       parent: unknown,
       args: { filterNeedle?: string; skip?: number; take?: number },
@@ -66,9 +76,11 @@ const resolvers = {
         value: args.take ?? 30,
       });
 
+      const skip = applySkipConstraints(args.skip ?? 0);
+
       return context.prisma.link.findMany({
         where,
-        skip: args.skip,
+        skip,
         take,
       });
     },
@@ -77,6 +89,11 @@ const resolvers = {
         where: { id: parseInt(args.id) },
       });
     },
+    link: async (parent: unknown, args: { id: string }, context: GraphQLContext) => {
+      return context.prisma.link.findUnique({
+        where: { id: parseInt(args.id) },
+      });
+    }
   },
   Link: {
     id: (parent: Link) => parent.id,
@@ -112,11 +129,15 @@ const resolvers = {
       const linkId = parseIntSafe(args.linkId);
       if (linkId === null) {
         return Promise.reject(
-          createGraphQLError(`Cannot post common on non-existing link with id '${args.linkId}'.`),
+          new GraphQLError(`Cannot post common on non-existing link with id '${args.linkId}'.`),
         );
       }
 
-      const comment = await context.prisma.comment
+      if (!args.body || args.body.trim().length === 0) {
+        return Promise.reject(new GraphQLError(`Comment body cannot be empty.`));
+      }
+
+      const newComment = await context.prisma.comment
         .create({
           data: {
             body: args.body,
@@ -126,16 +147,32 @@ const resolvers = {
         .catch((err: unknown) => {
           if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2003') {
             return Promise.reject(
-              createGraphQLError(
+              new GraphQLError(
                 `Cannot post common on non-existing link with id '${args.linkId}'.`,
               ),
             );
           }
           return Promise.reject(err);
         });
-      return comment;
+        
+      return newComment;
     },
   },
+  Comment: {
+    id: (parent: { id: number }) => parent.id,
+    body: (parent: { body: string }) => parent.body,
+    link: async (parent: { linkId: number }, _: unknown, context: GraphQLContext) => {
+      if (!parent.linkId) {
+        return null;
+      }
+
+      return context.prisma.link.findUnique({
+        where: {
+          id: parent.linkId,
+        },
+      });
+    },
+  }
 };
 
 export const schema = createSchema({
