1cd7fd53b65b49726567202a1f361cfd4eb06c88
Laurin Quast <laurinquast@googlemail.com>
2024-11-08 09:59:41 +0100

    add test

--- packages/graphql-yoga/__tests__/plugin-hooks.spec.ts
+++ packages/graphql-yoga/__tests__/plugin-hooks.spec.ts
@@ -1,4 +1,4 @@
-import { createYoga, type Plugin } from '../src';
+import { createSchema, createYoga, type Plugin } from '../src';
 import { eventStream } from './utilities';
 
 test('onParams -> setResult to single execution result', async () => {
@@ -58,4 +58,50 @@ test('onParams -> setResult to event stream execution result', async () => {
     }
   }
   expect(counter).toBe(2);
+});
+
+test('context value identity stays the same in all hooks', async () => {
+  const contextValues = [] as Array<unknown>;
+  const yoga = createYoga({
+    schema: createSchema({ typeDefs: `type Query {a:String}` }),
+    plugins: [
+      {
+        onParams(ctx) {
+          contextValues.push(ctx.context);
+        },
+        onParse(ctx) {
+          contextValues.push(ctx.context);
+        },
+        onValidate(ctx) {
+          contextValues.push(ctx.context);
+        },
+        onContextBuilding(ctx) {
+          contextValues.push(ctx.context);
+          // mutate context
+          ctx.extendContext({ a: 1 } as Record<string, unknown>);
+          contextValues.push(ctx.context);
+        },
+        onExecute(ctx) {
+          contextValues.push(ctx.args.contextValue);
+        },
+        onResponse(ctx) {
+          contextValues.push(ctx.serverContext);
+        },
+      } satisfies Plugin,
+    ],
+  });
+
+  const response = await yoga.fetch('http://localhost/graphql', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({ query: '{__typename}' }),
+  });
+  expect(response.status).toEqual(200);
+  expect(await response.json()).toEqual({ data: { __typename: 'Query' } });
+  expect(contextValues).toHaveLength(7);
+  for (const value of contextValues) {
+    expect(value).toEqual(contextValues[0]);
+  }
 });
