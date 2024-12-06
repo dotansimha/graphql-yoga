34a062c8f6a2edd2226fc6b25fd10eb0e4caaecc
urigo <uri.goldshtein@gmail.com>
2024-12-06 14:53:58 +0200

    Add back Comment createdAt and sort by it

--- examples/hackernews/src/schema/base/resolvers/Link.ts
+++ examples/hackernews/src/schema/base/resolvers/Link.ts
@@ -3,6 +3,7 @@ import type { LinkResolvers } from '../../types.generated';
 export const Link: LinkResolvers = {
   comments: async (parent, _arg, context) => {
     const comments = await context.prisma.comment.findMany({
+      orderBy: { createdAt: 'desc' },
       where: {
         linkId: parent.id,
       },
