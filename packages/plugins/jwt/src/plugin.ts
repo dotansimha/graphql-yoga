ca9d18baf08f61fd0cb0d8948275b1cda98c5d0b
jjangga0214 <bnbcmindnpass@gmail.com>
2024-12-09 13:37:16 +0900

    refactor(@graphql-yoga/plugin-jwt): `JWTExtendContextFields` generic default

--- packages/plugins/jwt/src/plugin.ts
+++ packages/plugins/jwt/src/plugin.ts
@@ -7,10 +7,10 @@ import { GraphQLError } from 'graphql';
 import { badRequestError, unauthorizedError } from './utils.js';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type ExtendableJwtPayload<T extends Record<string, any> = Record<string, any>> = JwtPayload & T;
+type ExtendableJwtPayload<T extends Record<string, any> = Record<string, never>> = JwtPayload & T;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type JWTExtendContextFields<T extends Record<string, any> = Record<string, any>> = {
+export type JWTExtendContextFields<T extends Record<string, any> = Record<string, never>> = {
   payload: ExtendableJwtPayload<T>;
   token: {
     value: string;
