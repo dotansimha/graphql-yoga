60c7d391f08cdd884c50856ba65bb95d74e8358f
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-12-10 14:49:36 +0300

    Update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,9 +2,6 @@ module.exports = {
   root: true,
   extends: ['@theguild'],
   ignorePatterns: ['packages/render-graphiql/src/graphiql.ts'],
-  parserOptions: {
-    project: './tsconfig.json',
-  },
   overrides: [
     {
       files: ['packages/graphql-yoga/src/plugins/**/*.ts'],
