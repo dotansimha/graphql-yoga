81585309e8735f576fae50e838661c2398c76184
urigo <uri.goldshtein@gmail.com>
2024-12-12 11:59:29 +0200

    Tutorial - Update Link.comments return type

--- examples/hackernews/src/schema/base/resolvers/Link.ts
+++ examples/hackernews/src/schema/base/resolvers/Link.ts
@@ -1,16 +1,12 @@
 import type { LinkResolvers } from '../../types.generated';
 
 export const Link: LinkResolvers = {
-  comments: async (parent, _arg, context) => {
-    const comments = await context.prisma.comment.findMany({
+  comments: (parent, _arg, context) => {
+    return context.prisma.comment.findMany({
       orderBy: { createdAt: 'desc' },
       where: {
         linkId: parent.id,
       },
     });
-    if (comments.length === 0) {
-      return null;
-    }
-    return comments;
   },
 };
