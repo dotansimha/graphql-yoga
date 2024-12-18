4001faaf4fc2576002445f0c602c1ebee8d99594
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-12-18 17:14:46 +0800

    add v4 [skip ci]

--- website/src/app/v4/[[...slug]]/page.tsx
+++ website/src/app/v4/[[...slug]]/page.tsx
@@ -12,9 +12,9 @@ import {
 import json from '../../../../remote-files/v4.json';
 import { useMDXComponents } from '../../../mdx-components';
 // @ts-expect-error -- add types for .mdx
-import LegacyDocsBanner from '../../legacy-docs-banner.mdx';
-// @ts-expect-error -- add types for .mdx
 import CodegenCallout from '../../codegen-callout.mdx';
+// @ts-expect-error -- add types for .mdx
+import LegacyDocsBanner from '../../legacy-docs-banner.mdx';
 
 const { branch, docsPath, filePaths, repo, user } = json;
 
@@ -26,11 +26,73 @@ const { mdxPages, pageMap: _pageMap } = convertToPageMap({
 // @ts-expect-error -- ignore
 const v4Pages = _pageMap[0].children;
 
-const yogaPageMap = mergeMetaWithPageMap(v4Pages);
+const yogaPageMap = mergeMetaWithPageMap(v4Pages, {
+  index: 'Quick Start',
+  features: {
+    items: {
+      schema: 'GraphQL Schema',
+      graphiql: 'GraphiQL',
+      context: 'GraphQL Context',
+      'error-masking': 'Error Masking',
+      introspection: 'Introspection',
+      subscriptions: 'Subscriptions',
+      'file-uploads': 'File Uploads',
+      'defer-stream': 'Defer and Stream',
+      'request-batching': 'Request Batching',
+      cors: 'CORS',
+      'csrf-prevention': 'CSRF Prevention',
+      'parsing-and-validation-caching': 'Parsing and Validation Caching',
+      'response-caching': 'Response Caching',
+      'persisted-operations': 'Persisted Operations',
+      'automatic-persisted-queries': 'Automatic Persisted Queries',
+      'logging-and-debugging': 'Logging and Debugging',
+      'health-check': 'Health Check',
+      'sofa-api': 'REST API',
+      cookies: 'Cookies',
+      'apollo-federation': 'Apollo Federation',
+      'envelop-plugins': 'Plugins',
+      testing: 'Testing',
+      jwt: 'JWT',
+    },
+  },
+  integrations: {
+    items: {
+      'integration-with-aws-lambda': 'AWS Lambda',
+      'integration-with-cloudflare-workers': 'Cloudflare Workers',
+      'integration-with-gcp': 'Google Cloud Platform',
+      'integration-with-deno': 'Deno',
+      'integration-with-express': 'Express',
+      'integration-with-fastify': 'Fastify',
+      'integration-with-koa': 'Koa',
+      'integration-with-nestjs': 'NestJS',
+      'integration-with-nextjs': 'Next.js',
+      'integration-with-sveltekit': 'SvelteKit',
+      'integration-with-hapi': 'Hapi',
+      'integration-with-bun': 'Bun',
+      'integration-with-uwebsockets': 'µWebSockets.js',
+      'z-other-environments': 'Other Environments',
+    },
+  },
+  migration: {
+    items: {
+      'migration-from-apollo-server': 'Apollo Server',
+      'migration-from-express-graphql': 'Express GraphQL',
+      'migration-from-yoga-v1': 'Yoga v1',
+      'migration-from-yoga-v2': 'Yoga v2',
+      'migration-from-yoga-v3': 'Yoga v3',
+    },
+  },
+});
 
 export const pageMap = normalizePageMap(yogaPageMap);
 
-const { wrapper: Wrapper, ...components } = useMDXComponents();
+const { wrapper: Wrapper, ...components } = useMDXComponents({
+  $Tabs: Tabs,
+  Callout,
+  CodegenCallout,
+  Tab: Tabs.Tab,
+  Tabs,
+});
 
 export default async function Page(props: NextPageProps<'...slug'>) {
   const params = await props.params;
