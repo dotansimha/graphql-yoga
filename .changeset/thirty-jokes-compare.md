17eb81bd389ab449e96ba686285bc22846807f82
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-12-24 11:08:28 +0300

    Mark `createLRUCache` as an internal utility

---
+++ .changeset/thirty-jokes-compare.md
@@ -0,0 +1,6 @@
+---
+'graphql-yoga': patch
+---
+
+Mark `createLRUCache` utility as deprecated, and export it as `_createLRUCache` marking it as an
+internal utility
--- packages/graphql-yoga/src/utils/create-lru-cache.ts
+++ packages/graphql-yoga/src/utils/create-lru-cache.ts
@@ -1,18 +1,47 @@
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 import { LRUCache as LRU } from 'lru-cache';
 
+/**
+ * @deprecated In the next major, `createLRUCache` will be renamed to `_createLRUCache`,
+ * and the current `createLRUCache` will be removed.
+ */
+export const createLRUCache = _createLRUCache;
+
+/**
+ * @deprecated In the next major, `LRUCacheOptions` will be renamed to `_LRUCacheOptions`,
+ * and the current `LRUCacheOptions` will be removed.
+ */
+export type LRUCacheOptions = _LRUCacheOptions;
+
+/**
+ * @deprecated In the next major, `LRUCache` will be renamed to `_LRUCache`,
+ * and the current `LRUCache` will be removed.
+ */
+export type LRUCache<T extends {}> = _LRUCache<T>;
+
 const DEFAULT_MAX = 1024;
 const DEFAULT_TTL = 3_600_000;
 
-export type LRUCache<T extends {}> = LRU<string, T>;
-export interface LRUCacheOptions {
+/**
+ * @internal This is an internal utility, and you should use it with your own risk.
+ * This utility can have breaking changes in the future.
+ */
+export type _LRUCache<T extends {}> = LRU<string, T>;
+/**
+ * @internal This is an internal utility, and you should use it with your own risk.
+ * This utility can have breaking changes in the future.
+ */
+export interface _LRUCacheOptions {
   max?: number;
   ttl?: number;
 }
-
-export function createLRUCache<T extends {}>({
+/**
+ * @internal This is an internal utility, and you should use it with your own risk.
+ * This utility can have breaking changes in the future.
+ */
+export function _createLRUCache<T extends {}>({
   max = DEFAULT_MAX,
   ttl = DEFAULT_TTL,
-}: LRUCacheOptions = {}) {
+}: _LRUCacheOptions = {}) {
   return new LRU<string, T>({ max, ttl });
 }
