0572208a46b5aac59be30ca58fededd7d442d7e4
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-01-03 15:17:54 +0800

    upd

--- website/src/app/(landing-pages)/truly-extendable.tsx
+++ website/src/app/(landing-pages)/truly-extendable.tsx
@@ -1,14 +1,14 @@
 import { ComponentProps, FC } from 'react';
 import Image from 'next/image';
-import { cn, Heading, InfoCard, CallToAction } from '@theguild/components';
+import { CallToAction, cn, Heading, InfoCard, YogaIcon } from '@theguild/components';
 import apolloBadge from './icons/apollo-badge.svg';
+import safeLineBadge from './icons/safe-line-badge.svg';
 import serverLineBadge from './icons/server-line-badge.svg';
 import timerLineBadge from './icons/timer-line-badge.svg';
-import safeLineBadge from './icons/safe-line-badge.svg';
 
 const classes = {
-  card: cn('[&_h3]:text-white [&_p]:text-white/80 bg-green-900 rounded-2xl')
-}
+  card: cn('[&_h3]:text-white [&_p]:text-white/80 bg-green-900 rounded-2xl'),
+};
 
 export const TrulyExtendableSection: FC<ComponentProps<'section'>> = ({ className, ...props }) => {
   return (
@@ -25,11 +25,11 @@ export const TrulyExtendableSection: FC<ComponentProps<'section'>> = ({ classNam
           <Heading as="h2" size="sm">
             Truly extendable
           </Heading>
-          <p className='mt-4'>
+          <p className="mt-4">
             Highly extendable through Envelop plugins, allowing customization to fit any development
             needs.
           </p>
-          <div className='grid grid-cols-2 gap-6 my-12'>
+          <div className="grid grid-cols-2 gap-6 my-12">
             <InfoCard
               heading="Apollo Federation"
               icon={<Image src={apolloBadge} alt="" />}
@@ -42,7 +42,8 @@ export const TrulyExtendableSection: FC<ComponentProps<'section'>> = ({ classNam
               icon={<Image src={safeLineBadge} alt="" />}
               className={classes.card}
             >
-              Mitigates the risk of arbitrary GraphQL operations with robust persistence capabilities.
+              Mitigates the risk of arbitrary GraphQL operations with robust persistence
+              capabilities.
             </InfoCard>
             <InfoCard
               heading="Response caching"
@@ -62,10 +63,22 @@ export const TrulyExtendableSection: FC<ComponentProps<'section'>> = ({ classNam
           <CallToAction variant="primary" href="/docs/features/envelop-plugins">
             Learn more about Envelop Plugins
           </CallToAction>
+          <YogaIcon
+            className="absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 size-2/3"
+            stroke="white"
+            strokeWidth="0.2"
+            fill="url(#myGradient)"
+          />
+          <svg>
+            <defs>
+              <linearGradient id="myGradient" x1="0%" y1="0%" x2="100%" y2="100%">
+                <stop offset="0%" style={{ stopColor: '#86b6c1', stopOpacity: 0.8 }} />
+                <stop offset="100%" style={{ stopColor: '#4f96a6', stopOpacity: 1 }} />
+              </linearGradient>
+            </defs>
+          </svg>
         </div>
-        <div className='bg-red-500 basis-1/2'>
-
-        </div>
+        <div className="basis-1/2 relative"></div>
       </div>
     </section>
   );
