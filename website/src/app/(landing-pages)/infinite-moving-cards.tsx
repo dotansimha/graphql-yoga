522333965f652d592d81cf381643ee43d231aa7a
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-01-03 16:33:55 +0800

    add infinity moving cards

---
+++ website/src/app/(landing-pages)/infinite-moving-cards.tsx
@@ -0,0 +1,52 @@
+import { Children, ReactElement, ReactNode } from 'react';
+import { cn } from '@theguild/components';
+
+const TimeToSeconds = {
+  fast: '20s',
+  normal: '40s',
+  slow: '80s',
+};
+
+export function InfiniteMovingCards({
+  children,
+  direction = 'left',
+  speed = 'fast',
+  pauseOnHover = true,
+  className,
+}: {
+  children: ReactNode;
+  direction?: 'left' | 'right';
+  speed?: 'fast' | 'normal' | 'slow';
+  pauseOnHover?: boolean;
+  className?: string;
+}): ReactElement {
+  const content = Children.map(children, (child, index) => (
+    <li key={index} className="w-[350px] max-w-full shrink-0 md:w-[450px]">
+      {child}
+    </li>
+  ));
+
+  return (
+    <div
+      className={clsx(
+        'relative max-w-7xl overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]',
+        className,
+      )}
+      style={{
+        ['--animation-duration' as string]: TimeToSeconds[speed],
+        ['--animation-direction' as string]: direction === 'left' ? 'forwards' : 'reverse',
+      }}
+    >
+      <ul
+        className={clsx(
+          'flex min-w-full shrink-0 gap-4 py-4 w-max flex-nowrap',
+          'animate-scroll',
+          pauseOnHover && 'hover:[animation-play-state:paused]',
+        )}
+      >
+        {content}
+        {content}
+      </ul>
+    </div>
+  );
+}
--- website/tailwind.config.ts
+++ website/tailwind.config.ts
@@ -25,10 +25,17 @@ const config: Config = {
           from: { height: 'var(--radix-accordion-content-height)', opacity: '1' },
           to: { height: '0', opacity: '0' },
         },
+        scroll: {
+          to: {
+            transform: 'translate(calc(-50% - .5rem))',
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.5s ease',
         'accordion-up': 'accordion-up 0.5s ease',
+        scroll:
+          'scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite',
       },
     },
   },
