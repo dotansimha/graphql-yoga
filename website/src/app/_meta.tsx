2860c34294ea3f0607d0c8794d96f326a9234cf4
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-01-06 20:54:45 +0800

    rm comment

--- website/src/app/_meta.tsx
+++ website/src/app/_meta.tsx
@@ -1,24 +1,4 @@
 export default {
-  // '*': {
-  //   theme: {
-  //     bottomContent: function BottomContent() {
-  //       const { resolvedTheme } = useTheme();
-  //       const { route } = useRouter();
-  //       return (
-  //         <Giscus
-  //           // ensure giscus is reloaded when client side route is changed
-  //           key={route}
-  //           repo="dotansimha/graphql-yoga"
-  //           repoId="MDEwOlJlcG9zaXRvcnkxMTA4MTk5Mzk="
-  //           category="Docs Discussion"
-  //           categoryId="DIC_kwDOBpr6Y84CAquY"
-  //           mapping="pathname"
-  //           theme={resolvedTheme}
-  //         />
-  //       );
-  //     },
-  //   },
-  // },
   index: {
     type: 'page',
     display: 'hidden',
