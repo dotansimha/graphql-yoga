b006ae095aa4d8640c72725ad329a983b1f14855
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-02-19 03:32:30 +0300

    Fix tests

--- examples/hackernews/__integration-tests__/hackernews.spec.ts
+++ examples/hackernews/__integration-tests__/hackernews.spec.ts
@@ -12,9 +12,10 @@ describe('hackernews example integration', () => {
     yoga = createYoga({ schema, context: createContext });
 
     // migrate
-    await MigrateDev.new().parse([
-      `--schema=${path.resolve(__dirname, '..', 'prisma', 'schema.prisma')}`,
-    ]);
+    await MigrateDev.new().parse(
+      [`--schema=${path.resolve(__dirname, '..', 'prisma', 'schema.prisma')}`],
+      {},
+    );
 
     // seed
     const client = new PrismaClient();
@@ -29,11 +30,14 @@ describe('hackernews example integration', () => {
 
   afterAll(async () => {
     // drop
-    await DbDrop.new().parse([
-      `--schema=${path.resolve(__dirname, '..', 'prisma', 'schema.prisma')}`,
-      '--preview-feature', // DbDrop is an experimental feature
-      '--force',
-    ]);
+    await DbDrop.new().parse(
+      [
+        `--schema=${path.resolve(__dirname, '..', 'prisma', 'schema.prisma')}`,
+        '--preview-feature', // DbDrop is an experimental feature
+        '--force',
+      ],
+      {},
+    );
   });
 
   it('should get posts from feed', async () => {
