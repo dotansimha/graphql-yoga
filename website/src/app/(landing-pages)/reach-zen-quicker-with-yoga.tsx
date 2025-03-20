df5b4d6553173831842f5219306b7df40b471fa6
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-03-20 03:40:46 +0300

    Fix ESLint

--- website/src/app/(landing-pages)/reach-zen-quicker-with-yoga.tsx
+++ website/src/app/(landing-pages)/reach-zen-quicker-with-yoga.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ComponentProps, FC } from 'react';
 import { CallToAction, cn, Heading, ComparisonTable as Table } from '@theguild/components';
 import ComparisonTable from '../comparison-table.mdx';
